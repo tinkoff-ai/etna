d66efc070dee46afb2fe3135fc0323eab360790e
alex-hse-repository <55380696+alex-hse-repository@users.noreply.github.com>
2021-09-07 12:22:03 +0300

    Update etna/models/sarimax.py

--- etna/models/sarimax.py
+++ etna/models/sarimax.py
@@ -183,7 +183,7 @@ class _SARIMAXModel:
         exog_train = self._select_regressors(df)
         regressor_columns = None
         if not isinstance(exog_train, type(None)):
-            regressor_columns = exog_train.columns
+            regressor_columns = exog_train.columns.values
 
         if regressor_columns:
             addition_to_params = len(regressor_columns) * [0]
