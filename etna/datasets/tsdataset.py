36d852a9a9c5a09e69a3207bdf11b6e35012e089
d.a.bunin <d.a.bunin@tinkoff.ru>
2021-09-13 12:56:16 +0300

    Solve circular import problem

--- etna/datasets/tsdataset.py
+++ etna/datasets/tsdataset.py
@@ -5,12 +5,14 @@ from typing import Optional
 from typing import Sequence
 from typing import Tuple
 from typing import Union
+from typing import TYPE_CHECKING
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-from etna.transforms.base import Transform
+if TYPE_CHECKING:
+    from etna.transforms.base import Transform
 
 TTimestamp = Union[str, pd.Timestamp]
 
@@ -64,13 +66,13 @@ class TSDataset:
 
         self.transforms = None
 
-    def transform(self, transforms: Iterable[Transform]):
+    def transform(self, transforms: Iterable["Transform"]):
         """Apply given transform to the data."""
         self.transforms = transforms
         for transform in self.transforms:
             self.df = transform.transform(self.df)
 
-    def fit_transform(self, transforms: Iterable[Transform]):
+    def fit_transform(self, transforms: Iterable["Transform"]):
         """Fit and apply given transforms to the data."""
         self.transforms = transforms
         for transform in self.transforms:
