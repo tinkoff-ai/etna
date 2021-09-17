f9f5295cb90dc51007d6b487cbd07bce19ad0068
a.p.chikov <a.p.chikov@macbook-a.p.chikov>
2021-09-17 14:06:55 +0300

    Add macro/micro mode for power transformers

--- etna/transforms/power.py
+++ etna/transforms/power.py
@@ -11,13 +11,18 @@ class YeoJohnsonTransform(SklearnTransform):
     """YeoJohnsonTransform applies Yeo-Johns transformation to a DataFrame."""
 
     def __init__(
-        self, in_column: Optional[Union[str, List[str]]] = None, inplace: bool = True, standardize: bool = True
+        self,
+        in_column: Optional[Union[str, List[str]]] = None,
+        inplace: bool = True,
+        standardize: bool = True,
+        mode: str = "per-segment",
     ):
         self.standardize = standardize
         super().__init__(
             in_column=in_column,
             inplace=inplace,
             transformer=PowerTransformer(method="yeo-johnson", standardize=self.standardize),
+            mode=mode,
         )
 
 
@@ -25,13 +30,18 @@ class BoxCoxTransform(SklearnTransform):
     """BoxCoxTransform applies Box-Cox transformation to DataFrame."""
 
     def __init__(
-        self, in_column: Optional[Union[str, List[str]]] = None, inplace: bool = True, standardize: bool = True
+        self,
+        in_column: Optional[Union[str, List[str]]] = None,
+        inplace: bool = True,
+        standardize: bool = True,
+        mode: str = "per-segment",
     ):
         self.standardize = standardize
         super().__init__(
             in_column=in_column,
             inplace=inplace,
             transformer=PowerTransformer(method="box-cox", standardize=self.standardize),
+            mode=mode,
         )
 
 
