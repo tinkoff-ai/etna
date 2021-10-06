b704eb461b89f1f0468584c292727f4379f4a6b5
Julia Shenshina <yuliya.shenshina@phystech.edu>
2021-10-06 10:08:20 +0300

    Update ruptures version

--- etna/transforms/binseg.py
+++ etna/transforms/binseg.py
@@ -1,51 +1,11 @@
-from functools import lru_cache
-from typing import Any
 from typing import Optional
 
 from ruptures.base import BaseCost
-from ruptures.costs import cost_factory
 from ruptures.detection import Binseg
 from sklearn.linear_model import LinearRegression
 
 from etna.transforms.change_points_trend import ChangePointsTrendTransform
 from etna.transforms.change_points_trend import TDetrendModel
-
-
-class _Binseg(Binseg):
-    """Binary segmentation with lru_cache."""
-
-    def __init__(
-        self,
-        model: str = "l2",
-        custom_cost: Optional[BaseCost] = None,
-        min_size: int = 2,
-        jump: int = 5,
-        params: Any = None,
-    ):
-        """Initialize a Binseg instance.
-
-        Args:
-            model (str, optional): segment model, ["l1", "l2", "rbf",...]. Not used if ``'custom_cost'`` is not None.
-            custom_cost (BaseCost, optional): custom cost function. Defaults to None.
-            min_size (int, optional): minimum segment length. Defaults to 2 samples.
-            jump (int, optional): subsample (one every *jump* points). Defaults to 5 samples.
-            params (dict, optional): a dictionary of parameters for the cost instance.
-        """
-        if custom_cost is not None and isinstance(custom_cost, BaseCost):
-            self.cost = custom_cost
-        elif params is None:
-            self.cost = cost_factory(model=model)
-        else:
-            self.cost = cost_factory(model=model, **params)
-        self.min_size = max(min_size, self.cost.min_size)
-        self.jump = jump
-        self.n_samples = None
-        self.signal = None
-
-    @lru_cache(maxsize=None)
-    def single_bkp(self, start: int, end: int) -> Any:
-        """Run _single_bkp with lru_cache decorator."""
-        return self._single_bkp(start=start, end=end)
 
 
 class BinsegTrendTransform(ChangePointsTrendTransform):
@@ -95,7 +55,7 @@ class BinsegTrendTransform(ChangePointsTrendTransform):
         self.epsilon = epsilon
         super().__init__(
             in_column=in_column,
-            change_point_model=_Binseg(
+            change_point_model=Binseg(
                 model=self.model, custom_cost=self.custom_cost, min_size=self.min_size, jump=self.jump
             ),
             detrend_model=detrend_model,
