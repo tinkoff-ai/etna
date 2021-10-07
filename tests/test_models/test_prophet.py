9820f41f8d2704cc0721630d639048d3bf2dffa3
alex-hse-repository <alex-box.ru@mail.ru>
2021-10-07 15:56:35 +0300

    Add confidence intrvals to Prophet

--- tests/test_models/test_prophet.py
+++ tests/test_models/test_prophet.py
@@ -42,3 +42,26 @@ def test_run_with_reg(new_format_df, new_format_exog):
         assert True
     else:
         assert False
+
+
+def test_confidence_interval_run_insample(example_tsds):
+    model = ProphetModel(interval_width=1)
+    model.fit(example_tsds)
+    forecast = model.forecast(example_tsds, confidence_interval=True)
+    for segment in forecast.segments:
+        segment_slice = forecast[:, segment, :][segment]
+        assert {"target_lower", "target_upper", "target"}.issubset(segment_slice.columns)
+        assert (segment_slice["target_lower"] <= segment_slice["target"]).all()
+        assert (segment_slice["target"] <= segment_slice["target_upper"]).all()
+
+
+def test_confidence_interval_run_infuture(example_tsds):
+    model = ProphetModel(interval_width=1)
+    model.fit(example_tsds)
+    future = example_tsds.make_future(10)
+    forecast = model.forecast(future, confidence_interval=True)
+    for segment in forecast.segments:
+        segment_slice = forecast[:, segment, :][segment]
+        assert {"target_lower", "target_upper", "target"}.issubset(segment_slice.columns)
+        assert (segment_slice["target_lower"] <= segment_slice["target"]).all()
+        assert (segment_slice["target"] <= segment_slice["target_upper"]).all()
