d063b606225d4885a847e5d88a3c6019724edde5
martins0n <martinmail@mail.ru>
2021-10-11 18:16:53 +0300

    FIX: notebooks to skip

--- scripts/notebook_runner.py
+++ scripts/notebook_runner.py
@@ -8,9 +8,14 @@ CURRENT_PATH = Path(__file__)
 ROOT_PATH = CURRENT_PATH.parents[1]
 NOTEBOOKS_FOLDER = ROOT_PATH / "examples"
 
+NOTEBOOKS_TO_SKIP = ["NN_examples.ipynb"]
+
 
 def run_notebooks():
     for notebook_path in NOTEBOOKS_FOLDER.glob("*.ipynb"):
+        if notebook_path.name in NOTEBOOKS_TO_SKIP:
+            typer.echo(f"Skipping {notebook_path}")
+            continue
         typer.echo(f"Running {notebook_path}")
         shell(
             f"poetry run python -m jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --to notebook --execute {notebook_path}"
