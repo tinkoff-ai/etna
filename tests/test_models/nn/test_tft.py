db7ba73e10c2515ef76536c8d8468e40450b887c
Romantsov Nikolay <nsromantsov@edu.hse.ru>
2021-10-19 17:55:40 +0300

    Fix tests

--- tests/test_models/nn/test_tft.py
+++ tests/test_models/nn/test_tft.py
@@ -45,13 +45,13 @@ def test_tft_model_run_weekly_overfit(weekly_period_df, horizon):
 
     ts_train = TSDataset(TSDataset.to_dataset(train), "D")
     ts_test = TSDataset(TSDataset.to_dataset(test), "D")
-    dft = DateFlagsTransform(day_number_in_week=True, day_number_in_month=False)
+    dft = DateFlagsTransform(day_number_in_week=True, day_number_in_month=False, out_column="dateflag")
     pft = PytorchForecastingTransform(
         max_encoder_length=21,
         min_encoder_length=21,
         max_prediction_length=horizon,
         time_varying_known_reals=["time_idx"],
-        time_varying_known_categoricals=["regressor_day_number_in_week"],
+        time_varying_known_categoricals=["dateflag_day_number_in_week"],
         time_varying_unknown_reals=["target"],
         static_categoricals=["segment"],
         target_normalizer=None,
