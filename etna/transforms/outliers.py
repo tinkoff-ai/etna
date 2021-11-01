61e228ce0b12dfc987f2b75e3448c4473f0a8138
Mr-Geekman <36005824+Mr-Geekman@users.noreply.github.com>
2021-11-01 14:07:20 +0300

    Add possibility to set custom `in_column` for `ConfidenceIntervalOutliersTransform` (#240)

--- etna/transforms/outliers.py
+++ etna/transforms/outliers.py
@@ -282,6 +282,7 @@ class ConfidenceIntervalOutliersTransform(OutliersTransform):
 
     def __init__(
         self,
+        in_column: str,
         model: Union[Type["ProphetModel"], Type["SARIMAXModel"]],
         interval_width: float = 0.95,
         **model_kwargs,
@@ -290,6 +291,8 @@ class ConfidenceIntervalOutliersTransform(OutliersTransform):
 
         Parameters
         ----------
+        in_column:
+            name of processed column
         model:
             model for confidence interval estimation
         interval_width:
@@ -297,12 +300,12 @@ class ConfidenceIntervalOutliersTransform(OutliersTransform):
 
         Notes
         -----
-        Works only with target column.
+        For not "target" column only column data will be used for learning.
         """
         self.model = model
         self.interval_width = interval_width
         self.model_kwargs = model_kwargs
-        super().__init__(in_column="target")
+        super().__init__(in_column=in_column)
 
     def detect_outliers(self, ts: TSDataset) -> Dict[str, List[pd.Timestamp]]:
         """Call `get_anomalies_confidence_interval` function with self parameters.
@@ -318,7 +321,7 @@ class ConfidenceIntervalOutliersTransform(OutliersTransform):
             dict of outliers in format {segment: [outliers_timestamps]}
         """
         return get_anomalies_confidence_interval(
-            ts=ts, model=self.model, interval_width=self.interval_width, **self.model_kwargs
+            ts=ts, model=self.model, interval_width=self.interval_width, in_column=self.in_column, **self.model_kwargs
         )
 
 
