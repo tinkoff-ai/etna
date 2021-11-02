79294afc14702c0097a00d6a92d3e8cffdc67ff6
alex-hse-repository <alex-box.ru@mail.ru>
2021-11-02 13:52:15 +0300

    Fix

--- tests/test_datasets/test_dataset.py
+++ tests/test_datasets/test_dataset.py
@@ -6,6 +6,7 @@ import pytest
 
 from etna.datasets import generate_ar_df
 from etna.datasets.tsdataset import TSDataset
+from etna.transforms import DateFlagsTransform
 
 
 @pytest.fixture()
@@ -51,7 +52,6 @@ def df_and_regressors() -> Tuple[pd.DataFrame, pd.DataFrame]:
     return df, df_exog
 
 
-'''
 def test_same_ending_error_raise():
     timestamp = pd.date_range("2021-01-01", "2021-02-01")
     df1 = pd.DataFrame({"timestamp": timestamp, "target": 11, "segment": "1"})
@@ -384,7 +384,6 @@ def test_right_format_sorting():
     inv_df = tsd.to_pandas(flatten=True)
     pd.testing.assert_series_equal(df["reg_1"], inv_df["reg_1"])
     pd.testing.assert_series_equal(df["reg_2"], inv_df["reg_2"])
-'''
 
 
 def test_to_flatten(example_df):
