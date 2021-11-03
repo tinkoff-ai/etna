3c1ad31c65a83ce3620ce44a5bf728876c48c5a6
Julia Shenshina <yuliya.shenshina@phystech.edu>
2021-11-03 18:29:40 +0300

    Omegaconf start

--- etna/commands/__main__.py
+++ etna/commands/__main__.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from omegaconf import OmegaConf
 from typer import Typer
 
 from etna.commands import backtest
@@ -6,6 +9,15 @@ from etna.commands import forecast
 app = Typer()
 app.command()(forecast)
 app.command()(backtest)
+
+
+def shift(steps_number: int, values: List[int]) -> List[int]:
+    """Shift values for steps_number steps."""
+    return [v + steps_number for v in values]
+
+
+OmegaConf.register_new_resolver("shift", shift)
+
 
 if __name__ == "__main__":
     app()
