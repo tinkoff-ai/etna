fcca133580af83cf7227ae62ac97134d6cd8c6c3
Julia Shenshina <yuliya.shenshina@phystech.edu>
2021-11-03 19:05:31 +0300

    Add mult

--- etna/commands/resolvers.py
+++ etna/commands/resolvers.py
@@ -4,3 +4,8 @@ from typing import List
 def shift(steps_number: int, values: List[int]) -> List[int]:
     """Shift values for steps_number steps."""
     return [v + steps_number for v in values]
+
+
+def mult(first: int, second: int) -> int:
+    """Multiply arguments."""
+    return first * second
