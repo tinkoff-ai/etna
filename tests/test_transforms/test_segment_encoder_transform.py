f423b728321de0e4c3bfe8e17cdac15a08aa76e8
alex-hse-repository <alex-box.ru@mail.ru>
2021-11-09 11:37:04 +0300

    Add test

--- tests/test_transforms/test_segment_encoder_transform.py
+++ tests/test_transforms/test_segment_encoder_transform.py
@@ -3,11 +3,11 @@ import pandas as pd
 import pytest
 
 from etna.datasets import TSDataset
+from etna.metrics import MAE
+from etna.models import LinearMultiSegmentModel
 from etna.transforms import MeanSegmentEncoderTransform
 from etna.transforms import SegmentEncoderTransform
-from etna.models import LinearMultiSegmentModel
-from etna.metrics import MAE,MAPE
-from etna.transforms import LagTransform
+
 
 @pytest.fixture
 def dummy_df() -> pd.DataFrame:
@@ -41,10 +41,10 @@ def simple_df() -> pd.DataFrame:
     df_2 = pd.DataFrame.from_dict({"timestamp": pd.date_range("2021-06-01", "2021-06-07", freq="D")})
     df_1["segment"] = "Moscow"
     df_1["target"] = [1.0, 2.0, 3.0, 4.0, 5.0, np.NAN, np.NAN]
-    df_1["exog"] = [6.0, 7.0, 8.0, 9.0, 10.0,11.,12.]
+    df_1["exog"] = [6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0]
     df_2["segment"] = "Omsk"
     df_2["target"] = [10.0, 20.0, 30.0, 40.0, 50.0, np.NAN, np.NAN]
-    df_2["exog"] = [60.0, 70.0, 80.0, 90.0, 100.0,110.,120.]
+    df_2["exog"] = [60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0]
     classic_df = pd.concat([df_1, df_2], ignore_index=True)
     df = TSDataset.to_dataset(classic_df)
     return df
@@ -56,11 +56,11 @@ def transformed_simple_df() -> pd.DataFrame:
     df_2 = pd.DataFrame.from_dict({"timestamp": pd.date_range("2021-06-01", "2021-06-07", freq="D")})
     df_1["segment"] = "Moscow"
     df_1["target"] = [1.0, 2.0, 3.0, 4.0, 5.0, np.NAN, np.NAN]
-    df_1["exog"] = [6.0, 7.0, 8.0, 9.0, 10.0,11.,12.]
+    df_1["exog"] = [6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0]
     df_1["regressor_segment_mean"] = [0, 1, 1.5, 2, 2.5, 3, 3]
     df_2["segment"] = "Omsk"
     df_2["target"] = [10.0, 20.0, 30.0, 40.0, 50.0, np.NAN, np.NAN]
-    df_2["exog"] = [60.0, 70.0, 80.0, 90.0, 100.0,110.,120.]
+    df_2["exog"] = [60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0]
     df_2["regressor_segment_mean"] = [0.0, 10.0, 15.0, 20.0, 25.0, 30, 30]
     classic_df = pd.concat([df_1, df_2], ignore_index=True)
     df = TSDataset.to_dataset(classic_df)
@@ -80,26 +80,25 @@ def test_mean_segment_encoder_transform(simple_df, transformed_simple_df):
     pd.testing.assert_frame_equal(transformed_df, transformed_simple_df)
 
 
-def test_mean_segment_encoder_transform(example_tsds):
+@pytest.fixture
+def almost_constant_ts(random_seed) -> TSDataset:
+    df_1 = pd.DataFrame.from_dict({"timestamp": pd.date_range("2021-06-01", "2021-07-01", freq="D")})
+    df_2 = pd.DataFrame.from_dict({"timestamp": pd.date_range("2021-06-01", "2021-07-01", freq="D")})
+    df_1["segment"] = "Moscow"
+    df_1["target"] = 1 + np.random.normal(0, 1, size=len(df_1))
+    df_2["segment"] = "Omsk"
+    df_2["target"] = 10 + np.random.normal(0, 1, size=len(df_1))
+    classic_df = pd.concat([df_1, df_2], ignore_index=True)
+    ts = TSDataset(df=TSDataset.to_dataset(classic_df), freq="D")
+    return ts
+
+
+def test_mean_segment_encoder_forecast(almost_constant_ts):
     horizon = 5
     model = LinearMultiSegmentModel()
-    lag = LagTransform(in_column="target", lags=[5])
     encoder = MeanSegmentEncoderTransform()
-    encoder2 = SegmentEncoderTransform()
 
-    train, test = example_tsds.train_test_split(test_size=horizon)
-    train.fit_transform([lag])
-    model.fit(train)
-    future = train.make_future(horizon)
-    pred = model.forecast(future)
-
-    train, test = example_tsds.train_test_split(test_size=horizon)
-    train.fit_transform([encoder2])
-    model.fit(train)
-    future = train.make_future(horizon)
-    pred_segment_encoding = model.forecast(future)
-
-    train, test = example_tsds.train_test_split(test_size=horizon)
+    train, test = almost_constant_ts.train_test_split(test_size=horizon)
     train.fit_transform([encoder])
     model.fit(train)
     future = train.make_future(horizon)
@@ -107,5 +106,4 @@ def test_mean_segment_encoder_transform(example_tsds):
 
     metric = MAE(mode="macro")
 
-    assert metric(pred_segment_encoding,test) > metric(pred_mean_segment_encoding,test)
-
+    assert metric(pred_mean_segment_encoding, test) < 1
