4bf5afe2c9f9090419a14447f729cac2c4b2b543
Andrey Alekseev <ilekseev@gmail.com>
2021-11-12 19:05:09 +0300

    Revert "Add MRMRFeatureSelectionTransform (#251)"
    This reverts commit 61661b6a

--- etna/transforms/feature_importance.py
+++ etna/transforms/feature_importance.py
@@ -8,16 +8,12 @@ from typing import Union
 import numpy as np
 import pandas as pd
 from catboost import CatBoostRegressor
-from mrmr import mrmr_classif
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.tree import ExtraTreeRegressor
 
-from etna.analysis import RelevanceTable
-from etna.clustering import EuclideanClustering
-from etna.clustering import HierarchicalClustering
 from etna.datasets import TSDataset
 from etna.transforms.base import Transform
 
@@ -108,111 +104,6 @@ class TreeFeatureSelectionTransform(Transform):
             return self
         weights = self._get_regressors_weights(df)
         self.selected_regressors = self._select_top_k_regressors(weights, self.top_k)
-        return self
-
-    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        Select top_k regressors.
-
-        Parameters
-        ----------
-        df:
-            dataframe with all segments data
-
-        Returns
-        -------
-        result: pd.DataFrame
-            Dataframe with with only selected regressors
-        """
-        result = df.copy()
-        selected_columns = sorted(
-            [
-                column
-                for column in df.columns.get_level_values("feature").unique()
-                if not column.startswith("regressor_") or column in self.selected_regressors
-            ]
-        )
-        result = result.loc[:, pd.IndexSlice[:, selected_columns]]
-        return result
-
-
-class MRMRFeatureSelectionTransform(Transform):
-    """Transform that selects regressors according to mRMR variable selection method."""
-
-    def __init__(
-        self,
-        relevance_method: RelevanceTable,
-        top_k: int,
-        clustering_method: HierarchicalClustering = EuclideanClustering(),
-        n_clusters: int = 10,
-        linkage: str = "average",
-        **relevance_params,
-    ):
-        """
-        Init MRMRFeatureSelectionTransform.
-
-        Parameters
-        ----------
-        relevance_method:
-            method to calculate relevance table
-        top_k:
-            num of regressors to select; if there are not enough regressors, then all will be selected
-        clustering_method:
-            method of time series clustering
-        n_clusters:
-            number of clusters
-        linkage:
-            rule for distance computation for new clusters, allowed "ward", "single", "average", "maximum", "complete"
-        """
-        if not isinstance(top_k, int) or top_k < 0:
-            raise ValueError("Parameter top_k should be positive integer")
-
-        if not isinstance(n_clusters, int) or n_clusters < 2:
-            raise ValueError("Parameter n_clusters should be integer and greater than 1")
-
-        self.relevance_method = relevance_method
-        self.clustering = clustering_method
-        self.n_clusters = n_clusters
-        self.linkage = linkage
-        self.top_k = top_k
-        self.relevance_params = relevance_params
-        self.selected_regressors: Optional[List[str]] = None
-
-    @staticmethod
-    def _get_regressors(df: pd.DataFrame) -> List[str]:
-        """Get list of regressors in the dataframe."""
-        result = set()
-        for column in df.columns.get_level_values("feature"):
-            if column.startswith("regressor_"):
-                result.add(column)
-        return sorted(list(result))
-
-    def fit(self, df: pd.DataFrame) -> "MRMRFeatureSelectionTransform":
-        """
-        Fit the method and remember features to select.
-
-        Parameters
-        ----------
-        df:
-            dataframe with all segments data
-
-        Returns
-        -------
-        result: MRMRFeatureSelectionTransform
-            instance after fitting
-        """
-        if len(self._get_regressors(df)) <= self.n_clusters:
-            raise ValueError("The number of clusters must be strictly less than the number of regressors")
-
-        ts = TSDataset(df=df, freq=pd.infer_freq(df.index))
-        self.clustering.build_distance_matrix(ts=ts)
-        self.clustering.build_clustering_algo(n_clusters=self.n_clusters, linkage=self.linkage)
-        s2c = self.clustering.fit_predict()
-        relevance_table = self.relevance_method(ts[:, :, "target"], ts[:, :, ts.regressors], **self.relevance_params)
-        y = np.empty(len(relevance_table))
-        for k, cluster in enumerate(relevance_table.index):
-            y[k] = s2c[cluster]
-        self.selected_regressors = mrmr_classif(relevance_table, y, K=self.top_k)
         return self
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
