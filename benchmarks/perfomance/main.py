99428550cf01b8c113d7d99d91f924a8b22bf657
martins0n <martinmail@mail.ru>
2021-11-24 01:14:19 +0300

    FIX: hydra

--- benchmarks/perfomance/main.py
+++ benchmarks/perfomance/main.py
@@ -1,64 +1,44 @@
-from pathlib import Path
-from typing import Any
-from typing import Dict
-from typing import Optional
+import json
+import pathlib
+import subprocess
+import threading
 
-import hydra_slayer
-import pandas as pd
-import typer
-from omegaconf import OmegaConf
+import hydra
+from omegaconf import DictConfig
 
-from etna.datasets import TSDataset
-from etna.pipeline import Pipeline
-from etna.datasets import generate_ar_df
+from scripts.utils import get_perfomance_dataframe_py_spy
 
-from scalene import scalene_profiler
-
-import time 
+FILE_FOLDER = pathlib.Path(__file__).parent.resolve()
 
 
-
-config_path = "configs/pipeline.yaml"
-backtest_config_path = "configs/backtest.yaml"
-pipeline_configs = OmegaConf.to_object(OmegaConf.load(config_path))
-backtest_configs = OmegaConf.to_object(OmegaConf.load(backtest_config_path))
-
-pipeline: Pipeline = hydra_slayer.get_from_params(**pipeline_configs)
-backtest_configs_hydra_slayer: Dict[str, Any] = hydra_slayer.get_from_params(**backtest_configs)
-
-#df_timeseries = pd.read_csv(target_path, parse_dates=["timestamp"])
-
-start_time = time.monotonic()
-df_timeseries = generate_ar_df(periods=100, start_time="2021-06-01", n_segments=100)
+def output_reader(proc):
+    for line in iter(proc.stdout.readline, b""):
+        print(line.decode("utf-8"), end="")
 
 
-df_timeseries = TSDataset.to_dataset(df_timeseries)
+@hydra.main(config_path="configs/", config_name="config")
+def bench(cfg: DictConfig) -> None:
+    proc = subprocess.Popen(
+        ["py-spy", "record", "-o", "speedscope.json", "-f", "speedscope", "python", FILE_FOLDER / "scripts" / "run.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    t = threading.Thread(target=output_reader, args=(proc,))
+    t.start()
+    print(proc)
+    proc.wait()
+
+    if proc.returncode != 0:
+        for i in proc.stderr.readlines():
+            print(i.decode())
+
+    with open("speedscope.json", "r") as f:
+        py_spy_dict = json.load(f)
+
+    df = get_perfomance_dataframe_py_spy(py_spy_dict)
+    df["line"] = df["line"].apply(lambda x: str(x).strip().replace("\\n", ""))
+    df.to_csv("py_spy.csv")
 
 
-df_exog = None
-
-
-tsdataset = TSDataset(df=df_timeseries, freq="1D", df_exog=df_exog)
-metrics, forecast, info = pipeline.backtest(ts=tsdataset, **backtest_configs_hydra_slayer)
-print((time.monotonic() - start_time))
-
-scalene_profiler.start()
-start_time = time.monotonic()
-
-df_timeseries = generate_ar_df(periods=100, start_time="2021-06-01", n_segments=100)
-
-
-df_timeseries = TSDataset.to_dataset(df_timeseries)
-
-
-df_exog = None
-
-
-tsdataset = TSDataset(df=df_timeseries, freq="1D", df_exog=df_exog)
-metrics, forecast, info = pipeline.backtest(ts=tsdataset, **backtest_configs_hydra_slayer)
-print((time.monotonic() - start_time))
-
-scalene_profiler.stop()
-
-
-# python3 -m scalene --off  --profile-all --profile-only etna-ts/etna,etna/etna --html --outfile=ll.html main.py configs/pipeline.yaml configs/backtest.yaml 
+if __name__ == "__main__":
+    bench()
