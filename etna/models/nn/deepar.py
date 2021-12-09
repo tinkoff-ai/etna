62f96d802d01cfb51b8e3be13ee8a6fc77053d5b
martins0n <martinmail@mail.ru>
2021-12-09 11:27:53 +0300

    FIX: docs model selection and nn

--- etna/models/nn/deepar.py
+++ etna/models/nn/deepar.py
@@ -20,6 +20,7 @@ if SETTINGS.torch_required:
 
 class DeepARModel(Model):
     """Wrapper for DeepAR from Pytorch Forecasting library.
+
     Notes
     -----
     We save TimeSeriesDataSet in instance to use it in the model.
