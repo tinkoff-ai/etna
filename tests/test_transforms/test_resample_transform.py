ce0e5af86e2d9a0e12f9b22d4ae7d6f812e7d344
Mr-Geekman <36005824+Mr-Geekman@users.noreply.github.com>
2021-12-09 10:42:53 +0300

    Add `TSDatasets.__init__` regressors logic (#357)

--- tests/test_transforms/test_resample_transform.py
+++ tests/test_transforms/test_resample_transform.py
@@ -58,7 +58,7 @@ def daily_exog_ts() -> Dict[str, Union[TSDataset, DistributionDict]]:
         }
     )
 
-    ts = TSDataset(df=TSDataset.to_dataset(df), freq="H", df_exog=TSDataset.to_dataset(df_exog))
+    ts = TSDataset(df=TSDataset.to_dataset(df), freq="H", df_exog=TSDataset.to_dataset(df_exog), known_future="all")
     distribution = {"segment_1": target1, "segment_2": target2}
     return {"ts": ts, "distribution": distribution}
 
@@ -104,7 +104,7 @@ def inplace_resampled_daily_exog_ts() -> TSDataset:
     )
     df_exog = pd.concat([df_exog1, df_exog2], ignore_index=True)
 
-    ts = TSDataset(df=TSDataset.to_dataset(df), freq="H", df_exog=TSDataset.to_dataset(df_exog))
+    ts = TSDataset(df=TSDataset.to_dataset(df), freq="H", df_exog=TSDataset.to_dataset(df_exog), known_future="all")
     return ts
 
 
@@ -144,7 +144,7 @@ def noninplace_resampled_daily_exog_ts() -> TSDataset:
     )
     df_exog = pd.concat([df_exog1, df_exog2], ignore_index=True)
 
-    ts = TSDataset(df=TSDataset.to_dataset(df), freq="H", df_exog=TSDataset.to_dataset(df_exog))
+    ts = TSDataset(df=TSDataset.to_dataset(df), freq="H", df_exog=TSDataset.to_dataset(df_exog), known_future="all")
     return ts
 
 
@@ -196,7 +196,7 @@ def weekly_exog_same_start_ts() -> Dict[str, Union[TSDataset, DistributionDict]]
         }
     )
     distribution = {"segment_1": target1, "segment_2": target2}
-    ts = TSDataset(df=TSDataset.to_dataset(df), freq="D", df_exog=TSDataset.to_dataset(df_exog))
+    ts = TSDataset(df=TSDataset.to_dataset(df), freq="D", df_exog=TSDataset.to_dataset(df_exog), known_future="all")
 
     return {"ts": ts, "distribution": distribution}
 
@@ -249,7 +249,7 @@ def weekly_exog_diff_start_ts() -> Dict[str, Union[TSDataset, DistributionDict]]
         }
     )
 
-    ts = TSDataset(df=TSDataset.to_dataset(df), freq="D", df_exog=TSDataset.to_dataset(df_exog))
+    ts = TSDataset(df=TSDataset.to_dataset(df), freq="D", df_exog=TSDataset.to_dataset(df_exog), known_future="all")
     distribution = {"segment_1": target1, "segment_2": target2}
     return {"ts": ts, "distribution": distribution}
 
