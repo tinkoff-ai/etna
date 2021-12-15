f7607bde87a3187cf4b2b7534a4d9000f71e6dc1
alex-hse-repository <alex-box.ru@mail.ru>
2021-12-15 15:20:51 +0300

    Update prophet

--- tests/test_models/test_prophet.py
+++ tests/test_models/test_prophet.py
@@ -59,3 +59,10 @@ def test_prediction_interval_run_infuture(example_tsds):
         segment_slice = forecast[:, segment, :][segment]
         assert {"target_0.025", "target_0.975", "target"}.issubset(segment_slice.columns)
         assert (segment_slice["target_0.975"] - segment_slice["target_0.025"] >= 0).all()
+
+
+def test_prophet_save_regressors_on_fit(example_reg_tsds):
+    model = ProphetModel()
+    model.fit(ts=example_reg_tsds)
+    for segment_model in model._models.values():
+        assert sorted(segment_model.regressor_columns) == example_reg_tsds.regressors
