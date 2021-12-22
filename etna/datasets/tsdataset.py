eaffb8bdb44c3500965d4d7191522180104bd0fe
d.a.bunin <d.a.bunin@tinkoff.ru>
2021-12-22 13:40:37 +0300

    Update example in describe

--- etna/datasets/tsdataset.py
+++ etna/datasets/tsdataset.py
@@ -779,7 +779,7 @@ class TSDataset:
         Examples
         --------
         >>> from etna.datasets import generate_const_df
-        >>> pd.options.display.width = 150
+        >>> pd.options.display.width = 500
         >>> df = generate_const_df(
         ...    periods=30, start_time="2021-06-01",
         ...    n_segments=2, scale=1
