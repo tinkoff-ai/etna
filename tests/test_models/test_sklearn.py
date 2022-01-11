5dd5441c1ad202a74c92a3adb537f8a0ab252c0b
alex-hse-repository <alex-box.ru@mail.ru>
2022-01-11 16:56:18 +0300

    Add test

--- tests/test_models/test_sklearn.py
+++ tests/test_models/test_sklearn.py
@@ -27,8 +27,23 @@ def test_sklearn_persegment_model_saves_regressors(ts_with_regressors, model):
         assert sorted(segment_model.regressor_columns) == sorted(ts_with_regressors.regressors)
 
 
+@pytest.mark.parametrize("model", ([SklearnPerSegmentModel(regressor=LinearRegression())]))
+def test_sklearn_persegment_model_regressors_number(ts_with_regressors, model):
+    """Test that the number of features used by SklearnPerSegmentModel is the same as the number of regressors."""
+    model.fit(ts_with_regressors)
+    for segment_model in model._models.values():
+        assert len(segment_model.model.coef_) == len(ts_with_regressors.regressors)
+
+
 @pytest.mark.parametrize("model", ([SklearnMultiSegmentModel(regressor=LinearRegression())]))
 def test_sklearn_multisegment_model_saves_regressors(ts_with_regressors, model):
     """Test that SklearnMultiSegmentModel saves the list of regressors from dataset on fit."""
     model.fit(ts_with_regressors)
     assert sorted(model._base_model.regressor_columns) == sorted(ts_with_regressors.regressors)
+
+
+@pytest.mark.parametrize("model", ([SklearnMultiSegmentModel(regressor=LinearRegression())]))
+def test_sklearn_multisegment_model_regressors_number(ts_with_regressors, model):
+    """Test that the number of features used by SklearnMultiSegmentModel is the same as the number of regressors."""
+    model.fit(ts_with_regressors)
+    assert len(model._base_model.model.coef_) == len(ts_with_regressors.regressors)
