f55c00940ba205df33efd69896efe9fe550baef9
martins0n <martinmail@mail.ru>
2022-01-13 14:12:22 +0300

    FIX: tsdataset doctest and frozenset elim

--- etna/ensembles/stacking_ensemble.py
+++ etna/ensembles/stacking_ensemble.py
@@ -203,11 +203,11 @@ class StackingEnsemble(Pipeline):
         features = pd.DataFrame()
         if self.filtered_features_for_final_model is not None:
             features_in_forecasts = [
-                set(forecast.columns.get_level_values("feature")).intersection(self.filtered_features_for_final_model)
+                list(set(forecast.columns.get_level_values("feature")).intersection(self.filtered_features_for_final_model))
                 for forecast in forecasts
             ]
             features = pd.concat(
-                [forecast[:, :, frozenset(features_in_forecasts[i])] for i, forecast in enumerate(forecasts)], axis=1
+                [forecast[:, :, features_in_forecasts[i]] for i, forecast in enumerate(forecasts)], axis=1
             )
             features = features.loc[:, ~features.columns.duplicated()]
         features_df = pd.concat([features, targets], axis=1)
