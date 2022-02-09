8f16589e9ae577ef46c67b73a5ac9fbf900cb312
d.a.bunin <d.a.bunin@tinkoff.ru>
2022-02-09 17:20:35 +0300

    Fix processing add_noise=True parameter in functions from datasets_generation

--- tests/test_datasets/test_datasets_generation.py
+++ tests/test_datasets/test_datasets_generation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from etna.datasets.datasets_generation import generate_ar_df
 from etna.datasets.datasets_generation import generate_const_df
@@ -6,43 +7,86 @@ from etna.datasets.datasets_generation import generate_from_patterns_df
 from etna.datasets.datasets_generation import generate_periodic_df
 
 
+def check_equals(generated_value, expected_value, **kwargs):
+    """Check that generated_value is equal to expected_value."""
+    return generated_value == expected_value
+
+
+def check_not_equal_within_3_sigma(generated_value, expected_value, sigma, **kwargs):
+    """Check that generated_value is not equal to expected_value, but withing 3 sigma range."""
+    if generated_value == expected_value:
+        return False
+    return abs(generated_value - expected_value) <= 3 * sigma
+
+
 def test_simple_ar_process_check():
     ar_coef = [10, 11]
     random_seed = 1
-    random_numbers = np.random.RandomState(seed=random_seed).normal(size=(2, 10))
-    ar_df = generate_ar_df(periods=10, start_time="2020-01-01", n_segments=2, ar_coef=ar_coef, random_seed=random_seed)
+    periods = 10
+    random_numbers = np.random.RandomState(seed=random_seed).normal(size=(2, periods))
+    ar_df = generate_ar_df(
+        periods=periods, start_time="2020-01-01", n_segments=2, ar_coef=ar_coef, random_seed=random_seed
+    )
 
-    assert len(ar_df) == 2 * 10
+    assert len(ar_df) == 2 * periods
     assert ar_df.iat[0, 2] == random_numbers[0, 0]
     assert ar_df.iat[1, 2] == ar_coef[0] * ar_df.iat[0, 2] + random_numbers[0, 1]
     assert ar_df.iat[2, 2] == ar_coef[1] * ar_df.iat[0, 2] + ar_coef[0] * ar_df.iat[1, 2] + random_numbers[0, 2]
 
 
-def test_simple_periodic_df_check():
+@pytest.mark.parametrize("add_noise, checker", [(False, check_equals), (True, check_not_equal_within_3_sigma)])
+def test_simple_periodic_df_check(add_noise, checker):
     period = 3
-    periodic_df = generate_periodic_df(periods=11, start_time="2020-01-01", n_segments=2, period=period)
-    assert len(periodic_df) == 2 * 11
-    assert periodic_df.iat[0, 2] == periodic_df.iat[0 + period, 2]
-    assert periodic_df.iat[1, 2] == periodic_df.iat[1 + period, 2]
-    assert periodic_df.iat[3, 2] == periodic_df.iat[3 + period, 2]
+    periods = 11
+    sigma = 0.1
+    periodic_df = generate_periodic_df(
+        periods=periods,
+        start_time="2020-01-01",
+        n_segments=2,
+        period=period,
+        add_noise=add_noise,
+        sigma=sigma,
+        random_seed=1,
+    )
+    assert len(periodic_df) == 2 * periods
+    diff_sigma = np.sqrt(2) * sigma
+    assert checker(periodic_df.iat[0, 2], periodic_df.iat[0 + period, 2], sigma=diff_sigma)
+    assert checker(periodic_df.iat[1, 2], periodic_df.iat[1 + period, 2], sigma=diff_sigma)
+    assert checker(periodic_df.iat[3, 2], periodic_df.iat[3 + period, 2], sigma=diff_sigma)
 
 
-def test_simple_const_df_check():
+@pytest.mark.parametrize("add_noise, checker", [(False, check_equals), (True, check_not_equal_within_3_sigma)])
+def test_simple_const_df_check(add_noise, checker):
     const = 1
-    const_df = generate_const_df(start_time="2020-01-01", n_segments=2, periods=3, scale=const)
-    assert len(const_df) == 2 * 3
-    assert const_df.iat[0, 2] == const
-    assert const_df.iat[1, 2] == const
-    assert const_df.iat[3, 2] == const
+    periods = 3
+    sigma = 0.1
+    const_df = generate_const_df(
+        start_time="2020-01-01",
+        n_segments=2,
+        periods=periods,
+        scale=const,
+        add_noise=add_noise,
+        sigma=sigma,
+        random_seed=1,
+    )
+    assert len(const_df) == 2 * periods
+    assert checker(const_df.iat[0, 2], const, sigma=sigma)
+    assert checker(const_df.iat[1, 2], const, sigma=sigma)
+    assert checker(const_df.iat[3, 2], const, sigma=sigma)
 
 
-def test_simple_from_patterns_df_check():
+@pytest.mark.parametrize("add_noise, checker", [(False, check_equals), (True, check_not_equal_within_3_sigma)])
+def test_simple_from_patterns_df_check(add_noise, checker):
     patterns = [[0, 1], [0, 2, 1]]
-    from_patterns_df = generate_from_patterns_df(start_time="2020-01-01", patterns=patterns, periods=10)
-    assert len(from_patterns_df) == len(patterns) * 10
-    assert from_patterns_df[from_patterns_df.segment == "segment_0"].iat[0, 2] == patterns[0][0]
-    assert from_patterns_df[from_patterns_df.segment == "segment_0"].iat[1, 2] == patterns[0][1]
-    assert from_patterns_df[from_patterns_df.segment == "segment_0"].iat[2, 2] == patterns[0][0]
-    assert from_patterns_df[from_patterns_df.segment == "segment_1"].iat[0, 2] == patterns[1][0]
-    assert from_patterns_df[from_patterns_df.segment == "segment_1"].iat[3, 2] == patterns[1][0]
-    assert from_patterns_df[from_patterns_df.segment == "segment_1"].iat[4, 2] == patterns[1][1]
+    periods = 10
+    sigma = 0.1
+    from_patterns_df = generate_from_patterns_df(
+        start_time="2020-01-01", patterns=patterns, periods=periods, add_noise=add_noise, sigma=sigma, random_seed=1
+    )
+    assert len(from_patterns_df) == len(patterns) * periods
+    assert checker(from_patterns_df[from_patterns_df.segment == "segment_0"].iat[0, 2], patterns[0][0], sigma=sigma)
+    assert checker(from_patterns_df[from_patterns_df.segment == "segment_0"].iat[1, 2], patterns[0][1], sigma=sigma)
+    assert checker(from_patterns_df[from_patterns_df.segment == "segment_0"].iat[2, 2], patterns[0][0], sigma=sigma)
+    assert checker(from_patterns_df[from_patterns_df.segment == "segment_1"].iat[0, 2], patterns[1][0], sigma=sigma)
+    assert checker(from_patterns_df[from_patterns_df.segment == "segment_1"].iat[3, 2], patterns[1][0], sigma=sigma)
+    assert checker(from_patterns_df[from_patterns_df.segment == "segment_1"].iat[4, 2], patterns[1][1], sigma=sigma)
