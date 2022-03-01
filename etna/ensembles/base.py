3cbf2612f19f129a5fb18d5e0508cd2cf60364ca
alex-hse-repository <alex-box.ru@mail.ru>
2022-03-01 10:46:22 +0300

    Update EnsembleMixin

--- etna/ensembles/base.py
+++ etna/ensembles/base.py
@@ -2,20 +2,20 @@ from typing import List
 
 from etna.datasets import TSDataset
 from etna.loggers import tslogger
-from etna.pipeline import Pipeline
+from etna.pipeline import BasePipeline
 
 
 class EnsembleMixin:
     """Base mixin for the ensembles."""
 
     @staticmethod
-    def _validate_pipeline_number(pipelines: List[Pipeline]):
+    def _validate_pipeline_number(pipelines: List[BasePipeline]):
         """Check that given valid number of pipelines."""
         if len(pipelines) < 2:
             raise ValueError("At least two pipelines are expected.")
 
     @staticmethod
-    def _get_horizon(pipelines: List[Pipeline]) -> int:
+    def _get_horizon(pipelines: List[BasePipeline]) -> int:
         """Get ensemble's horizon."""
         horizons = set([pipeline.horizon for pipeline in pipelines])
         if len(horizons) > 1:
@@ -23,7 +23,7 @@ class EnsembleMixin:
         return horizons.pop()
 
     @staticmethod
-    def _fit_pipeline(pipeline: Pipeline, ts: TSDataset) -> Pipeline:
+    def _fit_pipeline(pipeline: BasePipeline, ts: TSDataset) -> BasePipeline:
         """Fit given pipeline with ts."""
         tslogger.log(msg=f"Start fitting {pipeline}.")
         pipeline.fit(ts=ts)
@@ -31,7 +31,7 @@ class EnsembleMixin:
         return pipeline
 
     @staticmethod
-    def _forecast_pipeline(pipeline: Pipeline) -> TSDataset:
+    def _forecast_pipeline(pipeline: BasePipeline) -> TSDataset:
         """Make forecast with given pipeline."""
         tslogger.log(msg=f"Start forecasting with {pipeline}.")
         forecast = pipeline.forecast()
