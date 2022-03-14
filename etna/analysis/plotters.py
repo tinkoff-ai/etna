ac4e0d610cd512ea1cde2b2033fda0cac07e787e
d.a.bunin <d.a.bunin@tinkoff.ru>
2022-03-14 16:30:57 +0300

    Move legend to the right of the plot

--- etna/analysis/plotters.py
+++ etna/analysis/plotters.py
@@ -243,7 +243,7 @@ def plot_forecast(
                     )
         ax[i].set_title(segment)
         ax[i].tick_params("x", rotation=45)
-        ax[i].legend()
+        ax[i].legend(bbox_to_anchor=(1.02, 1.0))
 
 
 def plot_backtest(
