41cb17d5282ea4468a7c3f9597d05297d1d32364
martins0n <33594071+martins0n@users.noreply.github.com>
2022-04-07 19:08:57 +0300

    NEW: metrics for prediction intervals

---
+++ tests/test_metrics/test_intervals_metrics.py
@@ -0,0 +1,65 @@
+import pytest
+
+from etna.datasets import TSDataset
+from etna.metrics import Coverage
+from etna.metrics import Width
+
+
+@pytest.fixture
+def tsdataset_with_zero_width_quantiles(example_df):
+
+    ts_train = TSDataset.to_dataset(example_df)
+    ts_train = TSDataset(ts_train, freq="H")
+    example_df["target_0.025"] = example_df["target"]
+    example_df["target_0.975"] = example_df["target"]
+    ts_test = TSDataset.to_dataset(example_df)
+    ts_test = TSDataset(ts_test, freq="H")
+    return ts_train, ts_test
+
+
+@pytest.fixture
+def tsdataset_with_differnt_width_and_shifted_quantiles(example_df):
+
+    ts_train = TSDataset.to_dataset(example_df)
+    ts_train = TSDataset(ts_train, freq="H")
+    example_df["target_0.025"] = example_df["target"]
+    example_df["target_0.975"] = example_df["target"]
+
+    seggment_one_index = example_df[lambda x: x.segment == "segment_1"].index
+
+    example_df.loc[seggment_one_index, "target_0.025"] = example_df.loc[seggment_one_index, "target_0.025"] + 1
+    example_df.loc[seggment_one_index, "target_0.975"] = example_df.loc[seggment_one_index, "target_0.975"] + 2
+
+    ts_test = TSDataset.to_dataset(example_df)
+    ts_test = TSDataset(ts_test, freq="H")
+    return ts_train, ts_test
+
+
+def test_width_metric_with_zero_width_quantiles(tsdataset_with_zero_width_quantiles):
+    ts_train, ts_test = tsdataset_with_zero_width_quantiles
+
+    expected_metric = 0.0
+    width_metric = Width(mode="per-segment")(ts_train, ts_test)
+
+    for segment in width_metric:
+        assert width_metric[segment] == expected_metric
+
+
+def test_width_metric_with_differnt_width_and_shifted_quantiles(tsdataset_with_differnt_width_and_shifted_quantiles):
+    ts_train, ts_test = tsdataset_with_differnt_width_and_shifted_quantiles
+
+    expected_metric = {"segment_1": 1.0, "segment_2": 0.0}
+    width_metric = Width(mode="per-segment")(ts_train, ts_test)
+
+    for segment in width_metric:
+        assert width_metric[segment] == expected_metric[segment]
+
+
+def test_coverage_metric_with_differnt_width_and_shifted_quantiles(tsdataset_with_differnt_width_and_shifted_quantiles):
+    ts_train, ts_test = tsdataset_with_differnt_width_and_shifted_quantiles
+
+    expected_metric = {"segment_1": 0.0, "segment_2": 1.0}
+    coverage_metric = Coverage(mode="per-segment")(ts_train, ts_test)
+
+    for segment in coverage_metric:
+        assert coverage_metric[segment] == expected_metric[segment]
