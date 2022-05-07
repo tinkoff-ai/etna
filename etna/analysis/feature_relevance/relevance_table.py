d3bee4011a85a4223ff4798167e23fae31ddc45a
Artyom Makhin <amamakhin@yandex.ru>
2022-05-07 12:35:18 +0300

    fix get_statistics_relevance_table

--- etna/analysis/feature_relevance/relevance_table.py
+++ etna/analysis/feature_relevance/relevance_table.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Union
 
 import numpy as np
@@ -39,11 +40,31 @@ def get_statistics_relevance_table(df: pd.DataFrame, df_exog: pd.DataFrame) -> p
     regressors = sorted(df_exog.columns.get_level_values("feature").unique())
     segments = sorted(df.columns.get_level_values("segment").unique())
     result = np.empty((len(segments), len(regressors)))
+    none_warning = False
+    category_warning = False
     for k, seg in enumerate(segments):
         first_valid_idx = df.loc[:, seg].first_valid_index()
         df_now = df.loc[first_valid_idx:, seg]["target"]
-        df_exog_now = df_exog.loc[first_valid_idx:, seg]
-        relevance = calculate_relevance_table(df_exog_now[: len(df_now)], df_now)[["feature", "p_value"]].values
+        df_exog_now = df_exog.loc[first_valid_idx:, seg][: len(df_now)]
+        cat_cols = df_exog_now.dtypes[df_exog_now.dtypes == "category"].index
+        for cat_col in cat_cols:
+            try:
+                df_exog_now[cat_col] = df_exog_now[cat_col].astype(float)
+            except ValueError:
+                raise ValueError(f"{cat_col} column cannot be cast to float type! Please, use encoders.")
+        if len(cat_cols) > 0 and not category_warning:
+            category_warning = True
+            warnings.warn(
+                "Exogenous data contains columns with category type! It will be converted to float. If this is not desired behavior, use encoders."
+            )
+        df_exog_now["target"] = df_now
+        df_exog_now = df_exog_now.dropna()
+        if len(df_exog_now) != len(df_now) and not none_warning:
+            none_warning = True
+            warnings.warn("Exogenous or target data contains None! It will be dropped for calculating relevance.")
+        relevance = calculate_relevance_table(df_exog_now.drop(columns=["target"]), df_exog_now["target"])[
+            ["feature", "p_value"]
+        ].values
         result[k] = np.array(sorted(relevance, key=lambda x: x[0]))[:, 1]
     relevance_table = pd.DataFrame(result)
     relevance_table.index = segments
