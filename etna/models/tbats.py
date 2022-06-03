4ea68c894b58c993fb5132b8323e6709e9db5a59
ext.kvedernikov <ext.kvedernikov@NBUBNT457.tcsbank.ru>
2022-06-03 14:14:56 +0300

    issue tbats adapter

---
+++ etna/models/tbats.py
@@ -0,0 +1,51 @@
+from typing import List, Optional
+import pandas as pd
+from tbats import BATS, TBATS
+from etna.models.base import BaseAdapter, PerSegmentPredictionIntervalModel
+
+class _TBATSAdapter(BaseAdapter):
+    def __init__(self, model):
+        self.model = model
+        self.fitted_model = None
+
+    def fit(self, df: pd.DataFrame, regressors: List[str]):
+        target = df["target"]
+        self.fitted_model = self.model.fit(target)
+        return self
+
+    def predict(self, df: pd.DataFrame, prediction_interval, quantiles) -> pd.DataFrame:
+        y_pred = pd.DataFrame()
+        if prediction_interval:
+            for quantile in quantiles:
+                pred, confidence_intervals = self.fitted_model.forecast(steps=df.shape[0], confidence_level=quantile)
+                y_pred['target'] = pred
+                if quantile < 1/2:
+                    y_pred[f"target_{quantile:.4g}"] = confidence_intervals['lower_bound']
+                else:
+                    y_pred[f"target_{quantile:.4g}"] = confidence_intervals['upper_bound']
+        else:
+            pred = self.fitted_model.forecast(steps=df.shape[0])
+            y_pred['target'] = pred
+        return y_pred
+
+    def get_model(self):
+        return self.model
+
+
+class _TBATSPerSegmentModel(PerSegmentPredictionIntervalModel):
+    def __int__(self, model):
+        self.adapter = _TBATSAdapter(model)
+        super().__init__(base_model=self.adapter)
+
+
+class BATSPerSegmentModel(_TBATSPerSegmentModel):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.model = BATS(**kwargs)
+        super().__init__(_TBATSAdapter(self.model))
+
+class TBATSPerSegmentModel(_TBATSPerSegmentModel):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.model = TBATS(**kwargs)
+        super().__init__(_TBATSAdapter(self.model))
---
+++ tests/test_models/test_tbats.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import pytest
+import numpy as np
+from etna.datasets import TSDataset
+from etna.models.tbats import TBATSPerSegmentModel, BATSPerSegmentModel
+from etna.transforms import LagTransform
+from tests.test_models.test_linear_model import linear_segments_by_parameters
+from etna.metrics import MSE
+
+
+@pytest.fixture()
+def linear_segments_ts_unique(random_seed):
+    alpha_values = [np.random.rand() * 4 - 2 for _ in range(3)]
+    intercept_values = [np.random.rand() * 4 + 1 for _ in range(3)]
+    return linear_segments_by_parameters(alpha_values, intercept_values)
+
+
+@pytest.fixture()
+def sinusoid_ts():
+    horizon = 2
+    sinusoid_ts_1 = pd.DataFrame({'segment' : np.zeros(8), 'timestamp': pd.date_range(start='1/1/2018', end='1/08/2018'), 'target': [np.sin(i) for i in range(8)]})
+    sinusoid_ts_2 = pd.DataFrame({'segment' : np.ones(8), 'timestamp': pd.date_range(start='1/1/2018', end='1/08/2018'), 'target': [np.sin(i + 1) for i in range(8)]})
+    df = pd.concat((sinusoid_ts_1, sinusoid_ts_2))
+    df = TSDataset.to_dataset(df)
+    ts = TSDataset(df, freq="D")
+    return ts.train_test_split(test_size=horizon)
+
+@pytest.mark.parametrize(
+    "model_class, model_class_repr",
+    ((TBATSPerSegmentModel, "TBATSPerSegmentModel"), (BATSPerSegmentModel, "BATSPerSegmentModel")),
+)
+def test_reper(model_class, model_class_repr):
+     kwargs = {"seasonal_periods" : [14, 30.5]}
+     kwargs_repr = "seasonal_periods = [14, 30.5]"
+     model = model_class(**kwargs)
+     model_repr = model.__repr__()
+     true_repr = f"{model_class_repr}({kwargs_repr}, )"
+     assert model_repr == true_repr
+
+@pytest.mark.parametrize(
+     "model", (TBATSPerSegmentModel(), BATSPerSegmentModel())
+)
+def test_not_fitted(model, linear_segments_ts_unique):
+     train, test = linear_segments_ts_unique
+     to_forecast = train.make_future(3)
+     with pytest.raises(ValueError, match="model is not fitted!"):
+          model.forecast(to_forecast)
+
+@pytest.mark.parametrize(
+    "model", [TBATSPerSegmentModel(), BATSPerSegmentModel()]
+)
+def test_format(model, new_format_df):
+    df = new_format_df
+    ts = TSDataset(df, "1d")
+    lags = LagTransform(lags=[3, 4, 5], in_column="target")
+    ts.fit_transform([lags])
+    model.fit(ts)
+    future_ts = ts.make_future(3)
+    model.forecast(future_ts)
+    if not future_ts.isnull().values.any():
+        assert True
+    else:
+        assert False
+
+@pytest.mark.parametrize(
+    "model", [TBATSPerSegmentModel(), BATSPerSegmentModel()]
+)
+def test_dummy(model, sinusoid_ts):
+    train, test = sinusoid_ts
+    model.fit(train)
+    future_ts = train.make_future(2)
+    y_pred = model.forecast(future_ts)
+    metric = MSE(mode = 'macro')
+    value_metric = metric(y_pred, test)
+    assert value_metric < 4
+
+@pytest.mark.parametrize(
+    "model", [TBATSPerSegmentModel(), BATSPerSegmentModel()]
+)
+def test_prediction_interval(model, example_tsds):
+    model.fit(example_tsds)
+    forecast = model.forecast(example_tsds, prediction_interval=True, quantiles=[0.025, 0.975])
+    for segment in forecast.segments:
+        segment_slice = forecast[:, segment, :][segment]
+        assert {"target_0.025", "target_0.975", "target"}.issubset(segment_slice.columns)
+        assert (segment_slice["target_0.975"] - segment_slice["target_0.025"] >= 0).all()
