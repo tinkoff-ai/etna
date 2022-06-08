5afe83dc212725dc710c141a67666960c80f627e
ext.ytarasyuk <ext.ytarasyuk@nbubnt410.tcsbank.ru>
2022-06-08 12:59:29 +0300

    issue-632-change

--- etna/metrics/functional_metrics.py
+++ etna/metrics/functional_metrics.py
@@ -84,7 +84,10 @@ def smape(y_true: ArrayLike, y_pred: ArrayLike, eps: float = 1e-15) -> float:
 
 
 def sign(y_true: ArrayLike, y_pred: ArrayLike) -> float:
-    """Sign metric.
+    """Sign error metric.
+
+    .. math::
+        Sign(y\_true, y\_pred) = \\frac{1}{n}\\cdot\\sum_{i=0}^{n - 1}{sign(y\_true_i - y\_pred_i)}
 
     Parameters
     ----------
