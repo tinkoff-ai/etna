79037d9579467d700b15d0092098e65eabb13b48
alex-hse-repository <alex-box.ru@mail.ru>
2022-06-15 14:10:28 +0300

    Add SSM class

--- etna/models/nn/deepstate.py
+++ etna/models/nn/deepstate.py
@@ -1,3 +1,5 @@
+from abc import ABC
+from abc import abstractmethod
 from typing import Tuple
 from typing import TypedDict
 
@@ -9,6 +11,7 @@ from torch import Tensor
 
 class TrainBatch(TypedDict):
     encoder_real: Tensor
+    seasonal_indicator: Tensor
     target: Tensor
 
 
@@ -16,40 +19,109 @@ class InferenceBatch(TypedDict):
     encoder_real: Tensor
 
 
+class SSM(ABC):
+    @abstractmethod
+    def latent_dim(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def emission_coeff(self, time_index: Tensor) -> Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def transition_coeff(self) -> Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def innovation_coeff(self, time_index: Tensor) -> Tensor:
+        raise NotImplementedError
+
+
+class LevelSSM(SSM):
+    def latent_dim(self) -> int:
+        return 1
+
+    def emission_coeff(self, time_index: Tensor) -> Tensor:  # (batch_size, seq_length)
+        emission_coeff = torch.ones(time_index.shape[0], time_index.shape[1], self.latent_dim())
+        return emission_coeff  # (batch_size, seq_length, latent_dim)
+
+    def transition_coeff(self) -> Tensor:
+        transition_coeff = torch.eye(self.latent_dim())
+        return transition_coeff
+
+    def innovation_coeff(self, time_index: Tensor) -> Tensor:  # (batch_size, seq_length)
+        return self.emission_coeff(time_index)  # (batch_size, seq_length, latent_dim)
+
+
+class LevelTrendSSM(LevelSSM):
+    def latent_dim(self) -> int:
+        return 2
+
+    def transition_coeff(self) -> Tensor:
+        transition_coeff = torch.eye(self.latent_dim())
+        transition_coeff[0, 1] = 1
+        return transition_coeff
+
+
+class SeasonalitySSM(LevelSSM):
+    def __init__(self, num_seasons: int):
+        self.num_seasons = num_seasons
+
+    def latent_dim(self) -> int:
+        return self.num_seasons
+
+    def emission_coeff(self, time_index: Tensor) -> Tensor:  # (batch_size, seq_length)
+        emission_coeff = (
+            torch.nn.functional.one_hot(time_index, num_classes=self.latent_dim())
+            .reshape(-1, self.latent_dim(), 1)
+            .float()
+        )
+        return emission_coeff  # (batch_size, seq_length, latent_dim)
+
+    def innovation_coeff(self, time_index: Tensor) -> Tensor:  # (batch_size, seq_length)
+        return self.emission_coeff(time_index)  # (batch_size, seq_length, latent_dim)
+
+
 class DeepStateNetwork(LightningModule):
-    def __init__(self, input_size: int, seq_length: int,  latent_dim: int, num_layers: int = 1):
+    def __init__(self, ssm: SSM, input_size: int, seq_length: int, num_layers: int = 1):
         super(DeepStateNetwork).__init__()
-        self.num_layers = num_layers
+        self.ssm = ssm
+        self.latent_dim = self.ssm.latent_dim()
         self.input_size = input_size
-        self.latent_dim = latent_dim
+        self.num_layers = num_layers
         self.seq_length = seq_length
 
         self.RNN = nn.LSTM(
-            num_layers=self.num_layers, hidden_size=self.latent_dim, input_size=self.input_size, batch_first=False
+            num_layers=self.num_layers, hidden_size=self.latent_dim, input_size=self.input_size, batch_first=True
         )
         self.projectors = nn.ModuleDict(
             dict(
                 prior_mean=nn.Linear(in_features=self.latent_dim, out_features=self.latent_dim),
-                prior_cov=nn.Linear(in_features=self.latent_dim, out_features=self.latent_dim),
-                noise_std=nn.Linear(in_features=self.latent_dim, out_features=self.latent_dim),
-                innovation=nn.Linear(in_features=self.latent_dim, out_features=self.latent_dim),
-                residuals=nn.Linear(in_features=self.latent_dim, out_features=self.latent_dim),
+                prior_std=nn.Sequential(
+                    nn.Linear(in_features=self.latent_dim, out_features=self.latent_dim), nn.Softplus()
+                ),
+                innovation=nn.Sequential(
+                    nn.Linear(in_features=self.latent_dim, out_features=self.latent_dim), nn.Softplus()
+                ),
+                noise_std=nn.Sequential(nn.Linear(in_features=self.latent_dim, out_features=1), nn.Softplus()),
             )
         )
 
+
     def training_step(self, train_batch: TrainBatch, batch_idx):
-        encoder_real = train_batch["encoder_real"].float().permute(1, 0, 2)  # (encoder_length, batch_size, input_size)
-        targets = train_batch["target"].float().permute(1, 0)  # (encoder_length, batch_size)
+        encoder_real = train_batch["encoder_real"].float()  # (batch_size, encoder_length, input_size)
+        targets = train_batch["target"].float()  # (batch_size, encoder_length)
+        timestamps = train_batch["seasonal_indicator"]
 
         output, (_, _) = self.RNN(encoder_real)  # (batch_size, encoder_length, latent_dim)
+
         lds = LDS(
-            emission_coeff=self.projectors["emission_coeff"](output),
-            transition_coeff=self.projectors["transition_coeff"](output),
-            innovation_coeff=self.projectors["innovation_coeff"](output),
-            noise_std=self.projectors["noise_std"](output),
-            residuals=self.projectors["residuals"](output),
-            prior_mean=self.projectors["prior_mean"](output[:, 0]),
-            prior_cov=self.projectors["prior_cov"](output[:, 0]),
+            emission_coeff=self.ssm.emission_coeff(timestamps),  # (batch_size, encoder_length, latent_dim, 1)
+            transition_coeff=self.ssm.transition_coeff(),  # (latent_dim, latent_dim)
+            innovation_coeff=self.ssm.innovation_coeff(timestamps),  # (batch_size, encoder_length, latent_dim, 1)
+            noise_std=self.projectors["noise_std"](output),  # (batch_size, encoder_length, latent_dim, 1)
+            prior_mean=self.projectors["prior_mean"](output[:, 0]),  # (batch_size, latent_dim, 1)
+            prior_cov=self.projectors["prior_std"](output[:, 0]),  # (batch_size, latent_dim, latent_dim)
             seq_length=self.seq_length,
             latent_dim=self.latent_dim,
         )
@@ -67,7 +139,6 @@ class LDS:
         transition_coeff: Tensor,
         innovation_coeff: Tensor,
         noise_std: Tensor,
-        residuals: Tensor,
         prior_mean: Tensor,
         prior_cov: Tensor,
         seq_length: int,
@@ -77,7 +148,6 @@ class LDS:
         self.transition_coeff = transition_coeff
         self.innovation_coeff = innovation_coeff
         self.noise_std = noise_std
-        self.residuals = residuals
         self.prior_mean = prior_mean
         self.prior_cov = prior_cov
         self.seq_length = seq_length
@@ -86,7 +156,6 @@ class LDS:
     def kalman_filter_step(
         self,
         target: Tensor,
-        residual: Tensor,
         noise_std: Tensor,
         prior_mean: Tensor,
         prior_cov: Tensor,
@@ -101,8 +170,6 @@ class LDS:
         ----------
         target
             Observations of the system output, shape (batch_size, output_dim)
-        residual
-            Residual component, shape (batch_size, output_dim)
         noise_std
             Standard deviation of the output noise, shape (batch_size, output_dim)
         prior_mean(mu_t)
@@ -122,22 +189,23 @@ class LDS:
         Tensor
             Filtered_covariance, shape (batch_size, latent_dim, latent_dim)
         """
-        # H * mu (batch_size, output_dim)
-        target_mean = torch.matmul(emission_coeff, prior_mean.unsqueeze(-1)).squeeze(-1)
-        # R (batch_size, output_dim, output_dim)
+        # H * mu (batch_size, 1)
+        target_mean = (emission_coeff.permute(0, 2, 1) @ prior_mean.unsqueeze(-1)).squeeze(-1)
+        # v (batch_size, 1)
+        residual = target - target_mean
+        # R (batch_size, 1, 1)
         noise_cov = torch.diag_embed(noise_std * noise_std)
-        # F (batch_size, output_dim, output_dim)
-        target_cov = torch.matmul(torch.matmul(emission_coeff, prior_cov), emission_coeff.permute(0, 2, 1)) + noise_cov
-        # K (batch_size, latent_dim, output_dim)
-        kalman_gain = torch.matmul(torch.matmul(prior_cov, emission_coeff.permute(0, 2, 1)), torch.inverse(target_cov))
+        # F (batch_size, 1, 1)
+        target_cov = emission_coeff.permute(0, 2, 1) @ prior_cov @ emission_coeff + noise_cov
+        # K (batch_size, latent_dim, 1)
+        kalman_gain = prior_cov @ emission_coeff @ torch.inverse(target_cov)
 
         # mu = mu_t + K * v (batch_size, latent_dim)
-        filtered_mean = prior_mean + torch.matmul(kalman_gain, residual.unsqueeze(-1)).squeeze(-1)
+        filtered_mean = (prior_mean.unsqueeze(-1) + kalman_gain @ residual).squeeze(-1)
         # P = (I - KH)P_t (batch_size, latent_dim, latent_dim)
-        filtered_cov = torch.matmul(torch.eye(self.latent_dim) - torch.matmul(kalman_gain, emission_coeff), prior_cov)
+        filtered_cov = (torch.eye(self.latent_dim) - kalman_gain @ emission_coeff.permute(0, 2, 1)) @ prior_cov
         # log-likelihood (batch_size, )
         log_p = torch.distributions.multivariate_normal.MultivariateNormal(target_mean, target_cov).log_prob(target)
-
         return log_p, filtered_mean, filtered_cov
 
     def kalman_filter(self, targets: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
@@ -146,7 +214,7 @@ class LDS:
         Parameters
         ----------
         targets
-            Observations, shape (seq_length, batch_size, output_dim)
+            Observations, shape (batch_size, seq_length, output_dim)
 
         Returns
         -------
@@ -159,6 +227,7 @@ class LDS:
             Covariance of p(l_T | l_{T-1}), where T is seq_length, with shape
             (batch_size, latent_dim, latent_dim)
         """
+        targets = targets.permute(1, 0, 2)
         log_p_seq = []
         mean = self.prior_mean
         cov = self.prior_cov
@@ -166,7 +235,6 @@ class LDS:
         for t in range(self.seq_length):
             log_p, filtered_mean, filtered_cov = self.kalman_filter_step(
                 target=targets[t],
-                residual=self.residuals[t],
                 noise_std=self.noise_std[t],
                 prior_mean=mean,
                 prior_cov=cov,
@@ -174,10 +242,10 @@ class LDS:
             )
 
             log_p_seq.append(log_p.unsqueeze(-1))
-            mean = torch.matmul(self.transition_coeff[t], filtered_mean.unsqueeze(-1)).squeeze(-1)
-            cov = torch.matmul(
-                torch.matmul(self.transition_coeff[t], filtered_cov), self.transition_coeff[t].permute(0, 2, 1)
-            ) + torch.matmul(self.innovation_coeff[t], self.innovation_coeff[t].permute(0, 2, 1))
+            mean = (self.transition_coeff @ filtered_mean.unsqueeze(-1)).squeeze(-1)
+            cov = self.transition_coeff @ filtered_cov @ self.transition_coeff.permute(0, 2, 1) + self.innovation_coeff[
+                t
+            ] @ self.innovation_coeff[t].permute(0, 2, 1)
 
         return torch.cat(log_p_seq), mean, cov
 
