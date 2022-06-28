8e1e2ca4f2a4cf3d76c7f0ca310a4443b0708d77
ext.kvedernikov <ext.kvedernikov@NBUBNT457.tcsbank.ru>
2022-06-28 10:27:58 +0300

    CHANGE: delete examples

--- etna/pipeline/assemble_pipelines.py
+++ etna/pipeline/assemble_pipelines.py
@@ -14,18 +14,6 @@ def assemble_pipelines(
 ) -> List[Pipeline]:
     """Create pipelines from input horizons and models or sequence of models.
 
-    Examples
-    --------
-    >>> from etna.models import ProphetModel
-    >>> from etna.transforms import TrendTransform
-    >>> from etna.models import LinearPerSegmentModel
-    >>> assemble_pipelines(ProphetModel(growth='linear'), [TrendTransform(in_column="target")], [1, 2])
-    [Pipeline(model=ProphetModel(growth='linear'), transforms=[TrendTransform(in_column="target")], horizon=1),
-     Pipeline(model=ProphetModel(growth='linear'), transforms=[TrendTransform(in_column="target")], horizon=2)]
-    >>> assemble_pipelines([ProphetModel(growth='linear'), LinearPerSegmentModel()], [TrendTransform(in_column="target")], [1, 2])
-    [Pipeline(model=ProphetModel(growth='linear'), transforms=[TrendTransform(in_column="target")], horizon=1),
-     Pipeline(model=LinearPerSegmentModel(), transforms=[TrendTransform(in_column="target")], horizon=2)]
-
     Parameters
     ----------
     models:
