8be71ef9cfafd5312ae7e0df93f7456b365e0fb1
ext.kvedernikov <ext.kvedernikov@NBUBNT457.tcsbank.ru>
2022-07-06 02:37:26 +0300

    CHANGE: behavoir of assemble pipelines, tests

--- etna/pipeline/assemble_pipelines.py
+++ etna/pipeline/assemble_pipelines.py
@@ -1,4 +1,6 @@
+import itertools
 from typing import List
+from typing import Optional
 from typing import Sequence
 from typing import Union
 
@@ -9,8 +11,8 @@ from etna.transforms import Transform
 
 def assemble_pipelines(
     models: Union[BaseModel, Sequence[BaseModel]],
-    transforms: Sequence[Transform],
-    horizons: Sequence[int],
+    transforms: Sequence[Union[Transform, Sequence[Optional[Transform]]]],
+    horizons: Union[int, Sequence[int]],
 ) -> List[Pipeline]:
     """Create pipelines from input horizons and models or sequence of models.
 
@@ -31,11 +33,24 @@ def assemble_pipelines(
     -------
     list of pipelines
     """
-    if isinstance(models, Sequence):
-        if len(models) != len(horizons):
-            raise ValueError("len of sequences model and horizons must be equal")
-        return [
-            Pipeline(model=model, transforms=transforms, horizon=horizon) for model, horizon in zip(models, horizons)
-        ]
-    else:
-        return [Pipeline(model=models, transforms=transforms, horizon=horizon) for horizon in horizons]
+    n_pipelines = 1
+    for transform in transforms:
+        if isinstance(transform, Sequence):
+            n_pipelines *= len(transform)
+
+    if isinstance(models, Sequence) and n_pipelines != len(models):
+        raise ValueError("len of models sequence is not equals to expected number of pipelines")
+
+    if isinstance(horizons, Sequence) and n_pipelines != len(horizons):
+        raise ValueError("len of horizons sequence is not equals to expected number of pipelines")
+
+    models = models if isinstance(models, Sequence) else [models for _ in range(n_pipelines)]
+    horizons = horizons if isinstance(horizons, Sequence) else [horizons for _ in range(n_pipelines)]
+    transfoms_seq = [transform if isinstance(transform, Sequence) else [transform] for transform in transforms]
+    transforms_prod = list(itertools.product(*transfoms_seq))
+    transforms_without_nans = [[elem for elem in transform if elem is not None] for transform in transforms_prod]
+
+    return [
+        Pipeline(model, transform, horizon)
+        for model, transform, horizon in zip(models, transforms_without_nans, horizons)
+    ]
--- tests/test_pipeline/test_assemble_pipelines.py
+++ tests/test_pipeline/test_assemble_pipelines.py
@@ -3,6 +3,7 @@ import pytest
 from etna.models import LinearPerSegmentModel
 from etna.pipeline import Pipeline
 from etna.pipeline import assemble_pipelines
+from etna.transforms import LagTransform
 from etna.transforms import TrendTransform
 
 
@@ -10,17 +11,58 @@ def test_not_equal_lengths():
     models = [LinearPerSegmentModel()]
     transforms = [TrendTransform(in_column="target")]
     horizons = [1, 2, 3]
-    with pytest.raises(ValueError, match="len of sequences model and horizons must be equal"):
+    with pytest.raises(ValueError):
         _ = assemble_pipelines(models, transforms, horizons)
 
 
 @pytest.mark.parametrize(
-    "models", [[LinearPerSegmentModel(), LinearPerSegmentModel(), LinearPerSegmentModel()], LinearPerSegmentModel()]
+    "models, transforms, horizons, expected_len",
+    [
+        (LinearPerSegmentModel(), [TrendTransform(in_column="target")], 1, 1),
+        (
+            LinearPerSegmentModel(),
+            [
+                TrendTransform(in_column="target"),
+                [LagTransform(lags=[1, 2, 3], in_column="target"), LagTransform(lags=[2, 3, 4], in_column="target")],
+            ],
+            [1, 2],
+            2,
+        ),
+        (
+            [LinearPerSegmentModel(), LinearPerSegmentModel()],
+            [
+                TrendTransform(in_column="target"),
+                [LagTransform(lags=[1, 2, 3], in_column="target"), LagTransform(lags=[2, 3, 4], in_column="target")],
+            ],
+            1,
+            2,
+        ),
+        (
+            [LinearPerSegmentModel(), LinearPerSegmentModel(), LinearPerSegmentModel()],
+            [
+                TrendTransform(in_column="target"),
+                [
+                    LagTransform(lags=[1, 2, 3], in_column="target"),
+                    LagTransform(lags=[2, 3, 4], in_column="target"),
+                    None,
+                ],
+            ],
+            [1, 2, 3],
+            3,
+        ),
+    ],
 )
-def test_output_pipelines(models):
-    transforms = [TrendTransform(in_column="target")]
-    horizons = [1, 2, 3]
+def test_output_pipelines(models, transforms, horizons, expected_len):
     pipelines = assemble_pipelines(models, transforms, horizons)
-    assert len(pipelines) == len(horizons)
+    assert len(pipelines) == expected_len
     for pipeline in pipelines:
         assert isinstance(pipeline, Pipeline)
+
+
+def test_none_in_tranforms():
+    models = [LinearPerSegmentModel(), LinearPerSegmentModel()]
+    transforms = [TrendTransform(in_column="target"), [LagTransform(lags=[1, 2, 3], in_column="target"), None]]
+    horizons = [1, 2]
+    expected_transforms_lens = {1, 2}
+    pipelines = assemble_pipelines(models, transforms, horizons)
+    assert {len(pipeline.transforms) for pipeline in pipelines} == expected_transforms_lens
