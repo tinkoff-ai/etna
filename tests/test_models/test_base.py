797ee813c086d96de99f0126ebff5058d8ea5ddf
martins0n <33594071+martins0n@users.noreply.github.com>
2022-07-18 20:36:40 +0300

    FIX: test cases

--- tests/test_models/test_base.py
+++ tests/test_models/test_base.py
@@ -1,7 +1,38 @@
 from unittest.mock import MagicMock
+from unittest.mock import Mock
+from unittest.mock import call
 from unittest.mock import patch
 
+import numpy as np
+import pytest
+import torch
+
 from etna.models.base import DeepBaseModel
+
+
+@pytest.fixture()
+def deep_base_model_mock():
+    model = MagicMock()
+    model.train_batch_size = 32
+    model.train_dataloader_params = {}
+    model.val_dataloader_params = {}
+    model.test_batch_size = 32
+    model.trainer_params = {}
+    model.split_params = {}
+    return model
+
+
+@pytest.fixture()
+def sized_torch_dataset_mock():
+    torch_dataset = MagicMock()
+    torch_dataset.__len__.return_value = 100
+    return torch_dataset
+
+
+@pytest.fixture()
+def unsized_torch_dataset_mock():
+    torch_dataset = Mock()
+    return torch_dataset
 
 
 def test_deep_base_model_fit_calls_check():
@@ -9,39 +40,102 @@ def test_deep_base_model_fit_calls_check():
     deep_base_model.raw_fit.assert_called_once()
 
 
+@pytest.mark.parametrize("loggers", [[], ["logger"]])
 @patch("etna.models.base.random_split", return_value=(MagicMock(), MagicMock()))
 @patch("etna.models.base.DataLoader")
 @patch("etna.models.base.Trainer")
-def test_deep_base_model_raw_fit_split_params(trainer, dataloader, random_split):
+def test_deep_base_model_raw_fit(
+    trainer, dataloader, random_split, deep_base_model_mock, sized_torch_dataset_mock, loggers
+):
+    deep_base_model_mock.trainer_params = {"logger": loggers}
+    DeepBaseModel.raw_fit(self=deep_base_model_mock, torch_dataset=sized_torch_dataset_mock)
+    trainer.assert_called_with(logger=loggers)
+    trainer.return_value.fit.assert_called_with(
+        deep_base_model_mock, train_dataloaders=dataloader.return_value, val_dataloaders=None
+    )
+    random_split.assert_not_called()
 
-    torch_dataset_size = 100
-    train_size = 0.8
-    self_mock = MagicMock()
 
-    self_mock.split_params = {"train_size": train_size, "torch_dataset_size": torch_dataset_size}
-    self_mock.train_batch_size = 32
-    self_mock.train_dataloader_params = {}
-    self_mock.val_dataloader_params = {}
-    self_mock.test_batch_size = 32
-    self_mock.trainer_params = {}
-
-    torch_dataset = MagicMock()
-    torch_dataset.__len__.return_value = torch_dataset_size
-
-    DeepBaseModel.raw_fit(self=self_mock, torch_dataset=torch_dataset)
-    trainer.return_value.fit.assert_called_once()
+def _test_deep_base_model_raw_fit_split_params(
+    trainer,
+    random_split,
+    dataloader,
+    deep_base_model_mock,
+    torch_dataset_mock,
+    torch_dataset_train_size,
+    torch_dataset_val_size,
+):
+    DeepBaseModel.raw_fit(self=deep_base_model_mock, torch_dataset=torch_dataset_mock)
+    trainer.return_value.fit.assert_called_with(
+        deep_base_model_mock, train_dataloaders=dataloader.return_value, val_dataloaders=dataloader.return_value
+    )
+    calls = [
+        call(random_split.return_value[0], batch_size=deep_base_model_mock.train_batch_size, shuffle=True),
+        call(random_split.return_value[1], batch_size=deep_base_model_mock.train_batch_size, shuffle=False),
+    ]
+    dataloader.assert_has_calls(calls)
     random_split.assert_called_with(
-        torch_dataset,
-        lengths=[int(torch_dataset_size * train_size), torch_dataset_size - int(torch_dataset_size * train_size)],
+        torch_dataset_mock,
+        lengths=[torch_dataset_train_size, torch_dataset_val_size],
         generator=None,
     )
 
 
+@patch("etna.models.base.random_split", return_value=(MagicMock(), MagicMock()))
 @patch("etna.models.base.DataLoader")
-def test_deep_base_model_raw_predict_call_eval_check(dataloader):
+@patch("etna.models.base.Trainer")
+def test_deep_base_model_raw_fit_split_params_with_sized_torch_dataset(
+    trainer, dataloader, random_split, deep_base_model_mock, sized_torch_dataset_mock
+):
+    torch_dataset_size = len(sized_torch_dataset_mock)
+    train_size = 0.8
+    torch_dataset_train_size = int(torch_dataset_size * train_size)
+    torch_dataset_val_size = torch_dataset_size - torch_dataset_train_size
+    deep_base_model_mock.split_params = {"train_size": train_size}
+    _test_deep_base_model_raw_fit_split_params(
+        trainer,
+        random_split,
+        dataloader,
+        deep_base_model_mock,
+        sized_torch_dataset_mock,
+        torch_dataset_train_size,
+        torch_dataset_val_size,
+    )
+
+
+@patch("etna.models.base.random_split", return_value=(MagicMock(), MagicMock()))
+@patch("etna.models.base.DataLoader")
+@patch("etna.models.base.Trainer")
+def test_deep_base_model_raw_fit_split_params_with_unsized_torch_dataset(
+    trainer, dataloader, random_split, deep_base_model_mock, unsized_torch_dataset_mock
+):
+    torch_dataset_size = 50
+    train_size = 0.8
+    torch_dataset_train_size = int(torch_dataset_size * train_size)
+    torch_dataset_val_size = torch_dataset_size - torch_dataset_train_size
+    deep_base_model_mock.split_params = {"train_size": train_size, "torch_dataset_size": torch_dataset_size}
+
+    _test_deep_base_model_raw_fit_split_params(
+        trainer,
+        random_split,
+        dataloader,
+        deep_base_model_mock,
+        unsized_torch_dataset_mock,
+        torch_dataset_train_size,
+        torch_dataset_val_size,
+    )
+
+
+@patch("etna.models.base.DataLoader")
+def test_deep_base_model_raw_predict_call(dataloader):
     self_mock = MagicMock()
-    DeepBaseModel.raw_predict(self=self_mock, torch_dataset=MagicMock())
+    batch = {"segment": ["segment1", "segment2"], "target": torch.Tensor([[1, 2], [3, 4]])}
+    dataloader.return_value = [batch]
+    self_mock.return_value = batch["target"]
+    predictions_dict = DeepBaseModel.raw_predict(self=self_mock, torch_dataset=MagicMock())
     self_mock.eval.assert_called_once()
+    np.testing.assert_allclose(predictions_dict[("segment1", "target")], batch["target"][0].numpy())
+    np.testing.assert_allclose(predictions_dict[("segment2", "target")], batch["target"][1].numpy())
 
 
 def test_deep_base_model_forecast_inverse_transform_call_check():
