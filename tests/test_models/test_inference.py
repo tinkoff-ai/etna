41fcce471bbf39ddd8067be9be7b4b24bda9947a
Mr-Geekman <36005824+Mr-Geekman@users.noreply.github.com>
2022-07-22 11:16:58 +0300

    Teach `BATS`/`TBATS` to work with in-sample, out-sample predictions correctly (#806)

--- tests/test_models/test_inference.py
+++ tests/test_models/test_inference.py
@@ -57,7 +57,7 @@ def _test_forecast_in_sample_suffix(ts, model, transforms):
     forecast_ts = TSDataset(df, freq="D")
     forecast_ts.transform(ts.transforms)
     forecast_ts.df.loc[:, pd.IndexSlice[:, "target"]] = np.NaN
-    forecast_ts.df = forecast_ts.df.iloc[5:]
+    forecast_ts.df = forecast_ts.df.iloc[6:]
     model.forecast(forecast_ts)
 
     # checking
@@ -105,6 +105,41 @@ def _test_forecast_out_sample_suffix(ts, model, transforms):
     assert_frame_equal(forecast_gap_df, forecast_full_df.iloc[2:])
 
 
+def _test_forecast_mixed_in_out_sample(ts, model, transforms):
+    # fitting
+    df = ts.to_pandas()
+    ts.fit_transform(transforms)
+    model.fit(ts)
+
+    # forecasting mixed in-sample and out-sample
+    future_ts = ts.make_future(5)
+    future_df = future_ts.to_pandas().loc[:, pd.IndexSlice[:, "target"]]
+    df_full = pd.concat((df, future_df))
+    forecast_full_ts = TSDataset(df=df_full, freq=future_ts.freq)
+    forecast_full_ts.transform(ts.transforms)
+    forecast_full_ts.df.loc[:, pd.IndexSlice[:, "target"]] = np.NaN
+    forecast_full_ts.df = forecast_full_ts.df.iloc[6:]
+    model.forecast(forecast_full_ts)
+
+    # forecasting only in sample
+    forecast_in_sample_ts = TSDataset(df, freq="D")
+    forecast_in_sample_ts.transform(ts.transforms)
+    forecast_in_sample_ts.df.loc[:, pd.IndexSlice[:, "target"]] = np.NaN
+    forecast_in_sample_ts.df = forecast_in_sample_ts.df.iloc[6:]
+    model.forecast(forecast_in_sample_ts)
+
+    # forecasting only out sample
+    forecast_out_sample_ts = ts.make_future(5)
+    model.forecast(forecast_out_sample_ts)
+
+    # checking
+    forecast_full_df = forecast_full_ts.to_pandas()
+    forecast_in_sample_df = forecast_in_sample_ts.to_pandas()
+    forecast_out_sample_df = forecast_out_sample_ts.to_pandas()
+    assert_frame_equal(forecast_in_sample_df, forecast_full_df.iloc[:-5])
+    assert_frame_equal(forecast_out_sample_df, forecast_full_df.iloc[-5:])
+
+
 @pytest.mark.parametrize(
     "model, transforms",
     [
@@ -118,8 +153,6 @@ def _test_forecast_out_sample_suffix(ts, model, transforms):
         (MovingAverageModel(window=3), []),
         (NaiveModel(lag=3), []),
         (SeasonalMovingAverageModel(), []),
-        (BATSModel(use_trend=True), []),
-        (TBATSModel(use_trend=True), []),
     ],
 )
 def test_forecast_in_sample_full(model, transforms, example_tsds):
@@ -170,6 +203,18 @@ def test_forecast_in_sample_full_failed(model, transforms, example_tsds):
 @pytest.mark.parametrize(
     "model, transforms",
     [
+        (BATSModel(use_trend=True), []),
+        (TBATSModel(use_trend=True), []),
+    ],
+)
+def test_forecast_in_sample_full_not_implemented(model, transforms, example_tsds):
+    with pytest.raises(NotImplementedError, match="It is not possible to make in-sample predictions"):
+        _test_forecast_in_sample_full(example_tsds, model, transforms)
+
+
+@pytest.mark.parametrize(
+    "model, transforms",
+    [
         (CatBoostModelPerSegment(), [LagTransform(in_column="target", lags=[2, 3])]),
         (CatBoostModelMultiSegment(), [LagTransform(in_column="target", lags=[2, 3])]),
         (LinearPerSegmentModel(), [LagTransform(in_column="target", lags=[2, 3])]),
@@ -185,8 +230,6 @@ def test_forecast_in_sample_full_failed(model, transforms, example_tsds):
         (MovingAverageModel(window=3), []),
         (NaiveModel(lag=3), []),
         (SeasonalMovingAverageModel(), []),
-        (BATSModel(use_trend=True), []),
-        (TBATSModel(use_trend=True), []),
     ],
 )
 def test_forecast_in_sample_suffix(model, transforms, example_tsds):
@@ -227,6 +270,18 @@ def test_forecast_in_sample_suffix(model, transforms, example_tsds):
 )
 def test_forecast_in_sample_suffix_failed(model, transforms, example_tsds):
     _test_forecast_in_sample_suffix(example_tsds, model, transforms)
+
+
+@pytest.mark.parametrize(
+    "model, transforms",
+    [
+        (BATSModel(use_trend=True), []),
+        (TBATSModel(use_trend=True), []),
+    ],
+)
+def test_forecast_in_sample_suffix_not_implemented(model, transforms, example_tsds):
+    with pytest.raises(NotImplementedError, match="It is not possible to make in-sample predictions"):
+        _test_forecast_in_sample_suffix(example_tsds, model, transforms)
 
 
 @pytest.mark.parametrize(
@@ -305,6 +360,8 @@ def test_forecast_out_sample_prefix_failed(model, transforms, example_tsds):
         (HoltModel(), []),
         (HoltWintersModel(), []),
         (SimpleExpSmoothingModel(), []),
+        (BATSModel(use_trend=True), []),
+        (TBATSModel(use_trend=True), []),
         (
             TFTModel(max_epochs=1, learning_rate=[0.01]),
             [
@@ -333,8 +390,6 @@ def test_forecast_out_sample_suffix(model, transforms, example_tsds):
         (MovingAverageModel(window=3), []),
         (SeasonalMovingAverageModel(), []),
         (NaiveModel(lag=3), []),
-        (BATSModel(use_trend=True), []),
-        (TBATSModel(use_trend=True), []),
         (
             DeepARModel(max_epochs=5, learning_rate=[0.01]),
             [
@@ -351,3 +406,72 @@ def test_forecast_out_sample_suffix(model, transforms, example_tsds):
 )
 def test_forecast_out_sample_suffix_failed(model, transforms, example_tsds):
     _test_forecast_out_sample_suffix(example_tsds, model, transforms)
+
+
+@pytest.mark.parametrize(
+    "model, transforms",
+    [
+        (CatBoostModelPerSegment(), [LagTransform(in_column="target", lags=[5, 6])]),
+        (CatBoostModelMultiSegment(), [LagTransform(in_column="target", lags=[5, 6])]),
+        (LinearPerSegmentModel(), [LagTransform(in_column="target", lags=[5, 6])]),
+        (LinearMultiSegmentModel(), [LagTransform(in_column="target", lags=[5, 6])]),
+        (ElasticPerSegmentModel(), [LagTransform(in_column="target", lags=[5, 6])]),
+        (ElasticMultiSegmentModel(), [LagTransform(in_column="target", lags=[5, 6])]),
+        (ProphetModel(), []),
+        (HoltModel(), []),
+        (HoltWintersModel(), []),
+        (SimpleExpSmoothingModel(), []),
+    ],
+)
+def test_forecast_mixed_in_out_sample(model, transforms, example_tsds):
+    _test_forecast_mixed_in_out_sample(example_tsds, model, transforms)
+
+
+@pytest.mark.xfail(strict=True)
+@pytest.mark.parametrize(
+    "model, transforms",
+    [
+        (SARIMAXModel(), []),
+        (AutoARIMAModel(), []),
+        (
+            DeepARModel(max_epochs=5, learning_rate=[0.01]),
+            [
+                PytorchForecastingTransform(
+                    max_encoder_length=5,
+                    max_prediction_length=5,
+                    time_varying_known_reals=["time_idx"],
+                    time_varying_unknown_reals=["target"],
+                    target_normalizer=GroupNormalizer(groups=["segment"]),
+                )
+            ],
+        ),
+        (
+            TFTModel(max_epochs=1, learning_rate=[0.01]),
+            [
+                PytorchForecastingTransform(
+                    max_encoder_length=21,
+                    min_encoder_length=21,
+                    max_prediction_length=5,
+                    time_varying_known_reals=["time_idx"],
+                    time_varying_unknown_reals=["target"],
+                    static_categoricals=["segment"],
+                    target_normalizer=None,
+                )
+            ],
+        ),
+    ],
+)
+def test_forecast_mixed_in_out_sample_failed(model, transforms, example_tsds):
+    _test_forecast_mixed_in_out_sample(example_tsds, model, transforms)
+
+
+@pytest.mark.parametrize(
+    "model, transforms",
+    [
+        (BATSModel(use_trend=True), []),
+        (TBATSModel(use_trend=True), []),
+    ],
+)
+def test_forecast_mixed_in_out_sample_not_implemented(model, transforms, example_tsds):
+    with pytest.raises(NotImplementedError, match="It is not possible to make in-sample predictions"):
+        _test_forecast_mixed_in_out_sample(example_tsds, model, transforms)
