122ea36d61b243853e9d4ed5242dd6af93962343
ext.ytarasyuk <ext.ytarasyuk@nbubnt410.tcsbank.ru>
2022-08-01 23:39:14 +0300

    fix behavior of deadline model in big horizons

--- etna/models/deadline_ma.py
+++ etna/models/deadline_ma.py
@@ -18,7 +18,7 @@ class SeasonalityMode(Enum):
     @classmethod
     def _missing_(cls, value):
         raise NotImplementedError(
-            f"{value} is not a valid {cls.__name__}. Only {', '.join([repr(m.value) for m in cls])} seasonality allowed."
+            f"{value} is not a valid {cls.__name__}. Only {', '.join([repr(m.value) for m in cls])} seasonality allowed"
         )
 
 
@@ -46,14 +46,12 @@ class _DeadlineMovingAverageModel:
     def fit(self, df: pd.DataFrame, regressors: List[str]) -> "_DeadlineMovingAverageModel":
         """
         Fit DeadlineMovingAverageModel model.
-
         Parameters
         ----------
         df: pd.DataFrame
             Data to fit on
         regressors:
             List of the columns with regressors(ignored in this model)
-
         Raises
         ------
         ValueError
@@ -74,20 +72,21 @@ class _DeadlineMovingAverageModel:
                 f"It uses only target series for predict/\n "
             )
         targets = df["target"]
-        self.timestamps = df["timestamp"]
+        timestamps = df["timestamp"]
 
         if self.seasonality == SeasonalityMode.month:
-            first_index = self.timestamps.iloc[-1] - pd.DateOffset(months=self.window)
+            first_index = timestamps.iloc[-1] - pd.DateOffset(months=self.window)
 
         elif self.seasonality == SeasonalityMode.year:
-            first_index = self.timestamps.iloc[-1] - pd.DateOffset(years=self.window)
+            first_index = timestamps.iloc[-1] - pd.DateOffset(years=self.window)
 
-        if first_index < self.timestamps.iloc[0]:
+        if first_index < timestamps.iloc[0]:
             raise ValueError(
                 "Given series is too short for chosen shift value. Try lower shift value, or give" "longer series."
             )
 
-        self.series = targets.loc[self.timestamps >= first_index]
+        self.series = targets.loc[timestamps >= first_index]
+        self.timestamps = timestamps.loc[timestamps >= first_index]
         self.shift = len(self.series)
 
         return self
@@ -95,36 +94,37 @@ class _DeadlineMovingAverageModel:
     def predict(self, df: pd.DataFrame) -> np.ndarray:
         """
         Compute predictions from a DeadlineMovingAverageModel.
-
         Parameters
         ----------
         df: pd.DataFrame
             Used only for getting the horizon of forecast and timestamps
-
         Returns
         -------
         :
             Array with predictions.
         """
         timestamps = df["timestamp"]
-        res = np.zeros((len(df), 1))
-        for w in range(1, self.window + 1):
-            if self.seasonality == SeasonalityMode.month:
-                prev_dates = timestamps - pd.DateOffset(months=w)
-            elif self.seasonality == SeasonalityMode.year:
-                prev_dates = timestamps - pd.DateOffset(years=w)
-            values = []
-            for date in prev_dates:
-                value = self.series.loc[self.timestamps == date].values
-                values.append(value)
+        index = pd.date_range(start=self.timestamps.iloc[0], end=timestamps.iloc[-1])
+        res = np.append(self.series.values, np.zeros(len(df)))
+        res = pd.DataFrame(res)
+        res.index = index
+        for i in range(len(self.series), len(res)):
+            for w in range(1, self.window+1):
+                if self.seasonality == SeasonalityMode.month:
+                    prev_date = res.index[i] - pd.DateOffset(months=w)
 
-            res += np.array(values)
+                elif self.seasonality == SeasonalityMode.year:
+                    prev_date = res.index[i] - pd.DateOffset(years=w)
+                if prev_date <= self.timestamps.iloc[-1]:
+                    res.loc[index[i]] += self.series.loc[self.timestamps == prev_date].values
+                else:
+                    res.loc[index[i]] += res.loc[prev_date].values
 
-        res = res / self.window
-        res = res.reshape(
-            len(df),
-        )
-        return res
+            res.loc[index[i]] = res.loc[index[i]] / self.window
+
+        res = res.values.reshape(len(res), )
+
+        return res[-len(df):]
 
 
 class DeadlineMovingAverageModel(PerSegmentModel):
