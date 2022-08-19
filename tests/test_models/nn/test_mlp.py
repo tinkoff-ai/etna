58b761a9a121fc7e78a544ea0934e4e8197a5ece
ext.ytarasyuk <ext.ytarasyuk@nbubnt410.tcsbank.ru>
2022-08-19 11:28:34 +0300

    update tests

--- tests/test_models/nn/test_mlp.py
+++ tests/test_models/nn/test_mlp.py
@@ -14,33 +14,8 @@ from etna.transforms import LagTransform
 from etna.transforms import StandardScalerTransform
 
 
-@pytest.mark.parametrize("horizon", [8, 13])
-def test_mlp_model_run_weekly_overfit_with_scaler(ts_dataset_weekly_function_with_horizon, horizon):
-
-    ts_train, ts_test = ts_dataset_weekly_function_with_horizon(horizon)
-    lag = LagTransform(in_column="target", lags=list(range(horizon, horizon + 4)))
-    fourier = FourierTransform(period=7, order=3)
-    std = StandardScalerTransform(in_column="target")
-    ts_train.fit_transform([std, lag, fourier])
-
-    decoder_length = 14
-    model = MLPModel(
-        input_size=10,
-        encoder_length=0,
-        hidden_size=[10, 10, 10, 10, 10],
-        decoder_length=decoder_length,
-        trainer_params=dict(max_epochs=1000),
-    )
-    future = ts_train.make_future(decoder_length)
-    model.fit(ts_train)
-    future = model.forecast(future, horizon=horizon)
-
-    mae = MAE("macro")
-    assert mae(ts_test, future) < 0.05
-
-
 @pytest.fixture()
-def example_df_with_lag(random_seed):
+def example_df_with_lag():
     df1 = pd.DataFrame()
     df1["timestamp"] = pd.date_range(start="2020-01-01", end="2020-02-01", freq="H")
     df1["segment"] = "segment_1"
@@ -54,6 +29,31 @@ def example_df_with_lag(random_seed):
     df2["lag"] = df2["target"].shift(1)
 
     return pd.concat([df1, df2], ignore_index=True)
+
+
+@pytest.mark.parametrize("horizon", [8, 13])
+def test_mlp_model_run_weekly_overfit_with_scaler(ts_dataset_weekly_function_with_horizon, horizon):
+
+    ts_train, ts_test = ts_dataset_weekly_function_with_horizon(horizon)
+    lag = LagTransform(in_column="target", lags=list(range(horizon, horizon + 4)))
+    fourier = FourierTransform(period=7, order=3)
+    std = StandardScalerTransform(in_column="target")
+    ts_train.fit_transform([std, lag, fourier])
+
+    decoder_length = 14
+    model = MLPModel(
+        input_size=10,
+        hidden_size=[10, 10, 10, 10, 10],
+        lr=1e-1,
+        decoder_length=decoder_length,
+        trainer_params=dict(max_epochs=100),
+    )
+    future = ts_train.make_future(decoder_length)
+    model.fit(ts_train)
+    future = model.forecast(future, horizon=horizon)
+
+    mae = MAE("macro")
+    assert mae(ts_test, future) < 0.05
 
 
 def test_mlp_make_samples(example_df_with_lag):
@@ -79,13 +79,17 @@ def test_mlp_make_samples(example_df_with_lag):
 
 def test_mlp_step():
     model = MLPNet(input_size=3, hidden_size=[1], lr=1e-2, loss=None, optimizer_params=None)
-    batch = {"decoder_real": torch.Tensor([1, 2, 3]), "decoder_target": torch.Tensor([1, 2, 3]), "segment": "A"}
+    batch = {
+        "decoder_real": torch.Tensor([[1, 2, 3], [1, 2, 3]]),
+        "decoder_target": torch.Tensor([1, 2, 3]),
+        "segment": "A",
+    }
     loss, decoder_target, output = model.step(batch)
     assert type(loss) == torch.Tensor
     assert type(decoder_target) == torch.Tensor
     assert torch.all(decoder_target == batch["decoder_target"])
     assert type(output) == torch.Tensor
-    assert output.shape == torch.Size([1])
+    assert output.shape == torch.Size([2, 1])
 
 
 def test_mlp_layers():
