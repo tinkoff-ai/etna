152c25d3dfb37bb268de7502986fd81f6ccf3980
ext.ytarasyuk <ext.ytarasyuk@nbubnt410.tcsbank.ru>
2022-08-25 11:56:50 +0300

    fix size in batch

--- tests/test_models/nn/test_mlp.py
+++ tests/test_models/nn/test_mlp.py
@@ -73,12 +73,13 @@ def test_mlp_make_samples(simple_df_relevance):
 
 
 def test_mlp_step():
-    model = MLPNet(input_size=3, hidden_size=[1], lr=1e-2, loss=nn.MSELoss(), optimizer_params=None)
+
     batch = {
         "decoder_real": torch.Tensor([[1, 2, 3], [1, 2, 3], [1, 2, 3]]),
-        "decoder_target": torch.Tensor([1, 2, 3]),
+        "decoder_target": torch.Tensor([[1], [2], [3]]),
         "segment": "A",
     }
+    model = MLPNet(input_size=3, hidden_size=[1], lr=1e-2, loss=nn.MSELoss(), optimizer_params=None)
     loss, decoder_target, output = model.step(batch)
     assert type(loss) == torch.Tensor
     assert type(decoder_target) == torch.Tensor
