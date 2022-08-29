697d4e8d77656cdc0ce02a284bce4bbfde1259bd
alex-hse-repository <alex-box.ru@mail.ru>
2022-08-29 15:19:55 +0300

    Fix to_flatten

--- etna/datasets/tsdataset.py
+++ etna/datasets/tsdataset.py
@@ -615,7 +615,6 @@ class TSDataset:
         3 2021-06-04     1.0  segment_0
         4 2021-06-05     1.0  segment_0
         """
-        columns = df.columns.get_level_values("feature").unique()
         segments = df.columns.get_level_values("segment").unique()
         dtypes = df.dtypes
         category_columns = dtypes[dtypes == "category"].index.get_level_values(1).unique()
@@ -624,6 +623,7 @@ class TSDataset:
                 raise ValueError("The only possible literal is 'all'")
         else:
             df = df.loc[:, pd.IndexSlice[segments, features]].copy()
+        columns = df.columns.get_level_values("feature").unique()
 
         # flatten dataframe
         df_dict = {}
