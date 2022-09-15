7a3d8511e4d3cf1acdbd6c27ca07e6e0576d98b1
martins0n <33594071+martins0n@users.noreply.github.com>
2022-09-15 16:05:08 +0300

    FIX: trim white space

--- etna/auto/pool/templates.py
+++ etna/auto/pool/templates.py
@@ -6,18 +6,18 @@ DEFAULT = [
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.MovingAverageModel', 'window': '${mult:${horizon},1}'}},
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.MovingAverageModel', 'window': '${mult:${horizon},2}'}},
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.MovingAverageModel', 'window': '${mult:${horizon},3}'}},
-    
+
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.SeasonalMovingAverageModel', 'seasonality': 7, 'window': 3}},
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.SeasonalMovingAverageModel', 'seasonality': '${horizon}', 'window': 3}},
-    
+
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.HoltWintersModel'}},
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.HoltWintersModel', 'damped_trend': True, 'seasonal': 'add', 'trend': 'add'}},
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.HoltWintersModel', 'damped_trend': False, 'seasonal': 'add', 'trend': 'add'}} ,
 
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.AutoARIMAModel'}},
-    
+
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.TBATSModel'}},
-    
+
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.LinearPerSegmentModel'}, 'transforms': [{'_target_': 'etna.transforms.StandardScalerTransform', 'in_column': 'target'}, {'_target_': 'etna.transforms.LagTransform', 'in_column': 'target', 'lags': '${shift:${horizon},[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]}'}]},
     {'_target_': 'etna.pipeline.Pipeline', 'horizon': '${__aux__.horizon}', 'model': {'_target_': 'etna.models.LinearMultiSegmentModel'}, 'transforms': [{'_target_': 'etna.transforms.StandardScalerTransform', 'in_column': 'target', 'mode': 'macro'}, {'_target_': 'etna.transforms.LagTransform', 'in_column': 'target', 'lags': '${shift:${horizon},[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]}'}]},
 
