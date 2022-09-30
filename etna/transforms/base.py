3e5d766aacd8cb156ef80cddae2fd57f0b6f88ff
alex-hse-repository <55380696+alex-hse-repository@users.noreply.github.com>
2022-09-30 10:18:26 +0300

    Fix `required_features` for reversible transforms (#969)

--- etna/transforms/base.py
+++ etna/transforms/base.py
@@ -12,6 +12,7 @@ from typing_extensions import Literal
 
 from etna.core import BaseMixin
 from etna.datasets import TSDataset
+from etna.transforms.utils import match_target_quantiles
 
 
 class FutureMixin:
@@ -208,6 +209,14 @@ class ReversibleTransform(Transform):
         """
         pass
 
+    def _get_inverse_transform_required_features(self, ts) -> Union[List[str], Literal["all"]]:
+        """Add the target quantiles for the list with required features if necessary."""
+        required_features = self.required_features
+        if isinstance(required_features, list) and "target" in self.required_features:
+            features = set(ts.columns.get_level_values("feature").tolist())
+            required_features = list(set(required_features) | match_target_quantiles(features))
+        return required_features
+
     def inverse_transform(self, ts: TSDataset) -> TSDataset:
         """Inverse transform TSDataset.
 
@@ -223,7 +232,7 @@ class ReversibleTransform(Transform):
         :
             TSDataset after applying inverse transformation.
         """
-        df = ts.to_pandas(flatten=False, features=self.required_features)
+        df = ts.to_pandas(flatten=False, features=self._get_inverse_transform_required_features(ts))
         columns_before = set(df.columns.get_level_values("feature"))
         df_transformed = self._inverse_transform(df=df)
         ts = self._update_dataset(ts=ts, columns_before=columns_before, df_transformed=df_transformed)
