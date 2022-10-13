691fabea108513f1c2d08a7a7339b4c7cb8ffac9
alex-hse-repository <alex-box.ru@mail.ru>
2022-10-13 15:41:38 +0300

    Add weasel feature extractor

--- etna/experimental/classification/utils.py
+++ etna/experimental/classification/utils.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 import numpy as np
+from typing_extensions import Literal
 
 
 def crop_nans_single_series(x: np.ndarray) -> np.ndarray:
@@ -7,4 +10,16 @@ def crop_nans_single_series(x: np.ndarray) -> np.ndarray:
         raise ValueError("Dataset contains the series all consists of NaN values!")
     first_non_nan_ind = np.where(~np.isnan(x))[0][0]
     x = x[first_non_nan_ind:]
+    return x
+
+
+def padd_single_series(
+    x: np.ndarray, expected_len: int, padding_value: Union[float, Literal["back_fill"]]
+) -> np.ndarray:
+    """Apply padding on a single series."""
+    x = x[-expected_len:]
+    history_len = len(x)
+    padding_len = expected_len - history_len
+    padding_value = x[0] if padding_value == "back_fill" else padding_value
+    x = np.pad(array=x, pad_width=(padding_len, 0), mode="constant", constant_values=padding_value)
     return x
