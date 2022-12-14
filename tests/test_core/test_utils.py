d6da6c2a5d5df4c61e46bda45ed81f34b766dcc1
d.a.bunin <d.a.bunin@tinkoff.ru>
2022-12-14 17:01:17 +0300

    Rename load to load_saved and remove it from public etna.core

--- tests/test_core/test_utils.py
+++ tests/test_core/test_utils.py
@@ -3,24 +3,24 @@ import tempfile
 
 import pytest
 
-from etna.core import load
+from etna.core import load_saved
 from etna.transforms import AddConstTransform
 
 
-def test_load_fail_file_not_found():
+def test_load_saved_fail_file_not_found():
     non_existent_path = pathlib.Path("archive.zip")
     with pytest.raises(FileNotFoundError):
-        load(non_existent_path)
+        load_saved(non_existent_path)
 
 
-def test_load_ok():
+def test_load_saved_ok():
     transform = AddConstTransform(in_column="target", value=10.0, inplace=False)
     with tempfile.TemporaryDirectory() as _temp_dir:
         temp_dir = pathlib.Path(_temp_dir)
         save_path = temp_dir / "transform.zip"
         transform.save(save_path)
 
-        new_transform = load(save_path)
+        new_transform = load_saved(save_path)
         assert type(new_transform) == type(transform)
         for attribute in ["in_column", "value", "inplace"]:
             assert getattr(new_transform, attribute) == getattr(transform, attribute)
