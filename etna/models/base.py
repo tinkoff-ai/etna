38385ef3bb6c90a71e6632faad3ef70b6d498adf
d.a.bunin <d.a.bunin@tinkoff.ru>
2023-01-19 11:12:51 +0300

    Make explicit cast to np.float64 in nns

--- etna/models/base.py
+++ etna/models/base.py
@@ -632,8 +632,7 @@ class DeepBaseModel(DeepBaseAbstractModel, SaveNNMixin, NonPredictionIntervalCon
         future_ts = ts.tsdataset_idx_slice(start_idx=self.encoder_length, end_idx=self.encoder_length + prediction_size)
         for (segment, feature_nm), value in predictions.items():
             # we don't want to change dtype after assignment, but there can happen cast to float32
-            dtype = future_ts.df.loc[:, pd.IndexSlice[segment, feature_nm]]
-            future_ts.df.loc[:, pd.IndexSlice[segment, feature_nm]] = value[:prediction_size, :].astype(dtype)
+            future_ts.df.loc[:, pd.IndexSlice[segment, feature_nm]] = value[:prediction_size, :].astype(np.float64)
 
         future_ts.inverse_transform()
 
