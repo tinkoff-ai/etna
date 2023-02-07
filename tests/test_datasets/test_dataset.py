a5afe07408cff6b72b2ba511681dcaca4f89883d
gooseit <s02190108@gse.cs.msu.ru>
2023-02-07 15:45:23 +0300

    fixed_typo_in_test_dataset_file

--- tests/test_datasets/test_dataset.py
+++ tests/test_datasets/test_dataset.py
@@ -623,7 +623,7 @@ def test_to_flatten_with_exog(df_and_regressors_flat):
     for column, dtype in dtypes.items():
         if dtype == "category":
             expected_df[column] = expected_df[column].astype(dtype)
-    # this logic woouldn't work in general case, here we use that all features' names start with 'r'
+    # this logic wouldn't work in general case, here we use that all features' names start with 'r'
     sorted_columns = ["timestamp", "segment", "target"] + sorted_columns[:-3]
     # reindex df to assert correct columns order
     expected_df = expected_df[sorted_columns]
