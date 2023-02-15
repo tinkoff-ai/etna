b3cafc7cfe7e7998b2d550e3d14ebca8dfdf8925
gooseit <s02190108@gse.cs.msu.ru>
2023-02-15 16:32:46 +0300

    lint_3

--- etna/auto/auto.py
+++ etna/auto/auto.py
@@ -41,7 +41,17 @@ class _Initializer(Protocol):
 
 class AutoBase:
     """Base Class for Auto and Tune, implementing core logic behind these classes."""
-
+    
+    def __init__(
+        self,
+        target_metric: Metric,
+        metric_aggregation: MetricAggregationStatistics = "mean"
+    ):
+        # this code is never executed, its single purpose is to help linter (and user reading code) infer parameters types
+        self.target_metric: Metric = target_metric
+        self.metric_aggregation: MetricAggregationStatistics = metric_aggregation
+        self._optuna: Optional[Optuna] = None
+    
     def summary(self) -> pd.DataFrame:
         """Get Auto trials summary."""
         if self._optuna is None:
