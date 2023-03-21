3add472b4fb69d6d5b90b200b359e9e671fc8dd7
d.a.bunin <d.a.bunin@tinkoff.ru>
2023-03-21 13:21:33 +0300

    style: reformat code

--- etna/transforms/utils.py
+++ etna/transforms/utils.py
@@ -1,8 +1,8 @@
-from etna.datasets.utils import inverse_transform_target_components  # noqa: F401
 import reprlib
 from typing import List
 from typing import Optional
 
+from etna.datasets.utils import inverse_transform_target_components  # noqa: F401
 from etna.datasets.utils import match_target_quantiles  # noqa: F401
 
 
