a7eddb39b07720eda2738d6b262f9bc451f680ea
brsnw250 <maxzherelo1@gmail.com>
2023-03-21 12:52:00 +0300

    added tests for sarima

--- tests/test_models/test_sarimax_model.py
+++ tests/test_models/test_sarimax_model.py
@@ -1,9 +1,11 @@
 from copy import deepcopy
 
+import numpy as np
 import pytest
 from statsmodels.tsa.statespace.sarimax import SARIMAXResultsWrapper
 
 from etna.models import SARIMAXModel
+from etna.models.sarimax import _SARIMAXAdapter
 from etna.pipeline import Pipeline
 from tests.test_models.utils import assert_model_equals_loaded_original
 
@@ -133,3 +135,105 @@ def test_forecast_1_point(example_tsds):
 def test_save_load(example_tsds):
     model = SARIMAXModel()
     assert_model_equals_loaded_original(model=model, ts=example_tsds, transforms=[], horizon=3)
+
+
+@pytest.mark.parametrize(
+    "components_method_name,in_sample", (("predict_components", True), ("forecast_components", False))
+)
+def test_decomposition_hamiltonian_repr_error(dfs_w_exog, components_method_name, in_sample):
+    train, test = dfs_w_exog
+    pred_df = train if in_sample else test
+
+    model = _SARIMAXAdapter(order=(2, 0, 0), seasonal_order=(1, 0, 0, 3), hamilton_representation=True)
+    model.fit(train, ["f1", "f2"])
+
+    components_method = getattr(model, components_method_name)
+
+    with pytest.raises(
+        ValueError, match="Prediction decomposition is not implemented for Hamilton representation of an ARMA!"
+    ):
+        _ = components_method(df=pred_df)
+
+
+@pytest.mark.parametrize(
+    "components_method_name,in_sample", (("predict_components", True), ("forecast_components", False))
+)
+@pytest.mark.parametrize(
+    "regressors, regressors_components",
+    (
+        (["f1", "f2"], ["target_component_f1", "target_component_f2"]),
+        (["f1"], ["target_component_f1"]),
+        ([], []),
+    ),
+)
+@pytest.mark.parametrize("trend", (None, "t"))
+def test_components_names(dfs_w_exog, regressors, regressors_components, trend, components_method_name, in_sample):
+    expected_components = regressors_components + ["target_component_sarima"]
+
+    train, test = dfs_w_exog
+    pred_df = train if in_sample else test
+
+    model = _SARIMAXAdapter(trend=trend)
+    model.fit(train, regressors)
+
+    components_method = getattr(model, components_method_name)
+    components = components_method(df=pred_df)
+
+    assert set(components.columns) == set(expected_components)
+
+
+@pytest.mark.long_2
+@pytest.mark.parametrize(
+    "components_method_name,in_sample", (("predict_components", True), ("forecast_components", False))
+)
+@pytest.mark.parametrize(
+    "mle_regression,time_varying_regression,regressors",
+    (
+        (True, False, ["f1", "f2"]),
+        (True, False, []),
+        (False, True, ["f1", "f2"]),
+        (False, False, ["f1", "f2"]),
+        (False, False, []),
+    ),
+)
+@pytest.mark.parametrize("trend", (None, "t"))
+@pytest.mark.parametrize("enforce_stationarity", (True, False))
+@pytest.mark.parametrize("enforce_invertibility", (True, False))
+@pytest.mark.parametrize("concentrate_scale", (True, False))
+@pytest.mark.parametrize("use_exact_diffuse", (True, False))
+def test_components_sum_up_to_target(
+    dfs_w_exog,
+    components_method_name,
+    in_sample,
+    mle_regression,
+    time_varying_regression,
+    trend,
+    enforce_stationarity,
+    enforce_invertibility,
+    concentrate_scale,
+    use_exact_diffuse,
+    regressors,
+):
+    train, test = dfs_w_exog
+
+    model = _SARIMAXAdapter(
+        order=(1, 1, 1),
+        seasonal_order=(1, 1, 0, 2),
+        trend=trend,
+        mle_regression=mle_regression,
+        time_varying_regression=time_varying_regression,
+        enforce_stationarity=enforce_stationarity,
+        enforce_invertibility=enforce_invertibility,
+        concentrate_scale=concentrate_scale,
+        use_exact_diffuse=use_exact_diffuse,
+    )
+    model.fit(train, regressors)
+
+    components_method = getattr(model, components_method_name)
+
+    pred_df = train if in_sample else test
+
+    pred = model.predict(pred_df, prediction_interval=False, quantiles=[])
+    components = components_method(df=pred_df)
+
+    np.testing.assert_allclose(np.sum(components.values, axis=1), np.squeeze(pred))
