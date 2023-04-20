54c41042ec162e5f71c7d0ff0f7e938b56a013be
d.a.bunin <d.a.bunin@tinkoff.ru>
2023-04-20 13:27:56 +0300

    docs: add prototype for publishing docs to github-pages

---
+++ scripts/create_versions_list.py
@@ -0,0 +1,24 @@
+import json
+import pathlib
+
+import git
+
+CURRENT_PATH = pathlib.Path(__file__)
+ROOT_PATH = CURRENT_PATH.parents[1]
+
+
+def main():
+    repo = git.Repo(ROOT_PATH)
+    gh_pages = repo.branches["gh-pages"].commit
+    directories = [x.path for x in gh_pages.trees]
+    versions = directories
+
+    target_dir = ROOT_PATH / "gh-pages"
+    target_dir.mkdir(parents=True)
+    target_file = target_dir / "versions.json"
+    with target_file.open("w") as f:
+        json.dump(versions, f)
+
+
+if __name__ == "__main__":
+    main()
