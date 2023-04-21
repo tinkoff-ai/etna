bea4257109275b142b51086bdfd4507997d2821d
d.a.bunin <d.a.bunin@tinkoff.ru>
2023-04-21 12:26:39 +0300

    fix: fix publishing

--- scripts/create_versions_list.py
+++ scripts/create_versions_list.py
@@ -4,7 +4,7 @@ import tempfile
 
 import git
 
-DOCUMENTATION_URL = "https://github.com/tinkoff-ai/etna-docs"
+DOCUMENTATION_URL = "https://github.com/tinkoff-ai/etna-docs.git"
 CURRENT_PATH = pathlib.Path(__file__)
 ROOT_PATH = CURRENT_PATH.parents[1]
 
@@ -16,11 +16,11 @@ def main():
         directories = [x.path for x in gh_pages.trees]
         versions = directories
 
-    # target_dir = ROOT_PATH / "gh-pages"
-    # target_dir.mkdir(parents=True)
-    # target_file = target_dir / "versions.json"
-    # with target_file.open("w") as f:
-    #     json.dump(versions, f)
+    target_dir = ROOT_PATH / "gh-pages"
+    target_dir.mkdir(parents=True)
+    target_file = target_dir / "versions.json"
+    with target_file.open("w") as f:
+        json.dump(versions, f)
 
 
 if __name__ == "__main__":
