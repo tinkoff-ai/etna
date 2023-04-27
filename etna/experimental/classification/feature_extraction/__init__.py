f87c9fac15fa942ac00a521a9c1bb00ec1b8ccb1
d.a.bunin <d.a.bunin@tinkoff.ru>
2023-04-27 18:25:29 +0300

    fix: fix checking classification extras

--- etna/experimental/classification/feature_extraction/__init__.py
+++ etna/experimental/classification/feature_extraction/__init__.py
@@ -1,3 +1,6 @@
-from etna.experimental.classification.feature_extraction.base import BaseTimeSeriesFeatureExtractor
-from etna.experimental.classification.feature_extraction.tsfresh import TSFreshFeatureExtractor
-from etna.experimental.classification.feature_extraction.weasel import WEASELFeatureExtractor
+from etna import SETTINGS
+
+if SETTINGS.classification_required:
+    from etna.experimental.classification.feature_extraction.base import BaseTimeSeriesFeatureExtractor
+    from etna.experimental.classification.feature_extraction.tsfresh import TSFreshFeatureExtractor
+    from etna.experimental.classification.feature_extraction.weasel import WEASELFeatureExtractor
