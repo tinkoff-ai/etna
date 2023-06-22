a027062168d0625d205c6af454364f6dde5762fc
alex-hse-repository <alex-box.ru@mail.ru>
2023-06-22 10:18:55 +0300

    Add fast_redandancy flag to mrmr

--- etna/analysis/feature_selection/mrmr_selection.py
+++ etna/analysis/feature_selection/mrmr_selection.py
@@ -1,3 +1,4 @@
+import warnings
 from enum import Enum
 from typing import List
 
@@ -26,6 +27,7 @@ def mrmr(
     relevance_table: pd.DataFrame,
     regressors: pd.DataFrame,
     top_k: int,
+    fast_redundancy: bool = False,
     relevance_aggregation_mode: str = AggregationMode.mean,
     redundancy_aggregation_mode: str = AggregationMode.mean,
     atol: float = 1e-10,
@@ -47,6 +49,9 @@ def mrmr(
         dataframe with regressors in etna format
     top_k:
         num of regressors to select; if there are not enough regressors, then all will be selected
+    fast_redundancy:
+        * True: compute redundancy only inside the the segments, time complexity O(top_k * n_segments * n_features * history_len)
+        * False: compute redundancy for all the pairs of segments, time complexity O(top_k * n_segments * n_features * history_len)
     relevance_aggregation_mode:
         the method for relevance values per-segment aggregation
     redundancy_aggregation_mode:
@@ -58,11 +63,13 @@ def mrmr(
     -------
     selected_features: List[str]
         list of ``top_k`` selected regressors, sorted by their importance
-
-    Notes
-    -----
-    Time complexity of this method is O(top_k * n_segments * n_features * history_len)
     """
+    if not fast_redundancy:
+        warnings.warn(
+            "`fast_redundancy=False` was added for backward compatibility and will be removed in etna 3.0.0.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
     relevance_aggregation_fn = AGGREGATION_FN[AggregationMode(relevance_aggregation_mode)]
     redundancy_aggregation_fn = AGGREGATION_FN[AggregationMode(redundancy_aggregation_mode)]
 
@@ -81,13 +88,33 @@ def mrmr(
         score_denominator = pd.Series(1, index=not_selected_features)
         if i > 0:
             last_selected_feature = selected_features[-1]
-            candidate_regressors = regressors.loc[
-                pd.IndexSlice[:], pd.IndexSlice[:, not_selected_features + [last_selected_feature]]
-            ]
+            last_selected_regressor = regressors.loc[pd.IndexSlice[:], pd.IndexSlice[:, last_selected_feature]]
+            not_selected_regressors = regressors.loc[pd.IndexSlice[:], pd.IndexSlice[:, not_selected_features]]
 
-            segment_redundancy = [candidate_regressors[segment].corr() for segment in segments]
+            if fast_redundancy:
+                segment_redundancy = pd.concat(
+                    [
+                        not_selected_regressors[segment].apply(
+                            lambda col: last_selected_regressor[segment].corrwith(col)  # noqa: B023
+                        )
+                        for segment in segments
+                    ]
+                ).abs()
+            else:
+                segment_redundancy = (
+                    not_selected_regressors.apply(lambda col: last_selected_regressor.corrwith(col))  # noqa: B023
+                    .abs()
+                    .groupby("feature")
+                    .apply(redundancy_aggregation_fn)
+                    .T.groupby("feature")
+                )
+
             redundancy_table.loc[not_selected_features, last_selected_feature] = (
-                pd.concat(segment_redundancy).apply(redundancy_aggregation_fn, axis=0).loc[not_selected_features]
+                segment_redundancy.apply(redundancy_aggregation_fn)
+                .clip(atol)
+                .fillna(np.inf)
+                .loc[not_selected_features]
+                .values.squeeze()
             )
 
             score_denominator = redundancy_table.loc[not_selected_features, selected_features].mean(axis=1)
