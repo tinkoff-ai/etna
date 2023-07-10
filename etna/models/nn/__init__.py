f2b4e12eb2da0ea872ae26076e07869aef29a221
malodetz <malo.detz@yandex.ru>
2023-07-10 13:03:01 +0300

    Fix lint

--- etna/models/nn/__init__.py
+++ etna/models/nn/__init__.py
@@ -4,7 +4,7 @@ if SETTINGS.torch_required:
     from etna.models.nn.deepar import DeepARModel
     from etna.models.nn.deepstate.deepstate import DeepStateModel
     from etna.models.nn.mlp import MLPModel
+    from etna.models.nn.patchts import PatchTSModel
     from etna.models.nn.rnn import RNNModel
     from etna.models.nn.tft import TFTModel
     from etna.models.nn.utils import PytorchForecastingDatasetBuilder
-    from etna.models.nn.patchts import PatchTSModel
