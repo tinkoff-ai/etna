890f708090a716e7341f935df94b272cb68d9fca
d.a.bunin <d.a.bunin@tinkoff.ru>
2023-07-24 09:37:47 +0300

    fix: fix doctests

--- etna/models/__init__.py
+++ etna/models/__init__.py
@@ -12,27 +12,19 @@ Models are used to make predictions. Let's look at the basic example of usage:
 >>> from etna.models import LinearPerSegmentModel
 >>>
 >>> df = generate_ar_df(periods=100, start_time="2021-01-01", ar_coef=[1/2], n_segments=2)
->>> ts = TSDataset(TSDataset.to_dataset(df), "D")
+>>> ts = TSDataset(TSDataset.to_dataset(df), freq="D")
 >>> lag_transform = LagTransform(in_column="target", lags=[3, 4, 5])
 >>> ts.fit_transform(transforms=[lag_transform])
->>> future_ts = ts.make_future(3)
+>>> future_ts = ts.make_future(future_steps=3, transforms=[lag_transform])
 >>> model = LinearPerSegmentModel()
 >>> model.fit(ts)
-LinearPerSegmentModel(fit_intercept = True, normalize = False, )
+LinearPerSegmentModel(fit_intercept = True, )
 >>> forecast_ts = model.forecast(future_ts)
-segment                 segment_0  ... segment_1
-feature    regressor_target_lag_3  ...    target
-timestamp                          ...
-2021-04-11              -0.090673  ...  0.286764
-2021-04-12              -0.665337  ...  0.295589
-2021-04-13               0.365363  ...  0.374554
-[3 rows x 8 columns]
+>>> forecast_ts is future_ts
+True
 
 There is a key note to mention: :code:`future_ts` and :code:`forecast_ts` are the same objects.
 Method :code:`forecast` only fills 'target' columns in :code:`future_ts` and return reference to it.
-
->>> forecast_ts is future_ts
-True
 """
 
 from etna import SETTINGS
